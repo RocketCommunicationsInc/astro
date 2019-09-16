af849aeb5527ff068ae8c309826ea0ec773b9743
Duncan McAlester <duncan@rocketcom.com>
2019-09-16 12:03:34 -0700

    Added eleventy config files

---
+++ packages/astro-uxds/.eleventy.js
@@ -0,0 +1,34 @@
+module.exports = function(eleventyConfig) {
+  const markdownIt = require('markdown-it');
+  const markdownItContainer = require('markdown-it-container');
+  const markdownItAttrs = require('markdown-it-attrs');
+  const cleanCSS = require('clean-css');
+
+  // markdown options
+
+  const options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+
+  const markdownLib = markdownIt(options)
+    .use(markdownItContainer, 'note')
+    .use(markdownItAttrs);
+  eleventyConfig.setLibrary('md', markdownLib);
+
+  // Manuall move static content
+  eleventyConfig.addPassthroughCopy('img');
+  eleventyConfig.addPassthroughCopy('js');
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('fonts');
+
+  // You can return your Config object (optional).
+  return {
+    dir: {
+      input: 'test',
+      data: '../_data',
+      includes: '../_includes',
+    },
+  };
+};
