9215081026124e1a2fdfc3b0b304c6dabb610e6f
Duncan McAlester <duncan@rocketcom.com>
2019-10-02 14:58:06 -0700

    Updated nav template to support expanding current section Added a data-section attribute to parent elements if it matches the current page URL it will expand the correct tree section.

--- packages/astro-uxds/js/navigation.js
+++ packages/astro-uxds/js/navigation.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('dom ready');
+document.addEventListener('DOMContentLoaded', () => {
+  const nav = document.getElementById('navigation');
+
+  nav.addEventListener('click', (e) => {
+    const selectedNavItem = e.target.parentElement;
+
+    selectedNavItem.setAttribute(
+      'aria-expanded',
+      selectedNavItem.getAttribute('aria-expanded') === 'false' ? 'true' : 'false'
+    );
+  });
 });
