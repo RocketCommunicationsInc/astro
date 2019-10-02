1a38c333927ec285433abe301592c98a3990d5b7
Duncan McAlester <duncan@rocketcom.com>
2019-10-02 15:32:21 -0700

    Scroll position of main nav set to past scroll position Adapted from Brian’s initial work on Astro 4.0, the scroll position of the left nav is stored in the users last scrolled position.

--- packages/astro-uxds/js/navigation.js
+++ packages/astro-uxds/js/navigation.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const storage = typeof Storage !== undefined;
   const nav = document.getElementById('navigation');
 
+  nav.scrollTop = (storage && sessionStorage.getItem('navScrollPos')) || 0;
+
+  // Listen for clicks isolated to the navigation element
   nav.addEventListener('click', (e) => {
+    // reference clicked element
     const selectedNavItem = e.target.parentElement;
 
-    selectedNavItem.setAttribute(
-      'aria-expanded',
-      selectedNavItem.getAttribute('aria-expanded') === 'false' ? 'true' : 'false'
-    );
+    console.log(storage);
+
+    // toggle expanded/collapsed aria state to hide show element
+    if (selectedNavItem.getAttribute('aria-expanded')) {
+      selectedNavItem.setAttribute(
+        'aria-expanded',
+        selectedNavItem.getAttribute('aria-expanded') === 'false' ? 'true' : 'false'
+      );
+    } else {
+      if (storage) {
+        sessionStorage.setItem('navScrollPos', nav.scrollTop);
+      }
+    }
   });
 });
