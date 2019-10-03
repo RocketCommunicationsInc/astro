516e337e5789597c7ed6ff37e06ecbc14e6d4fd6
Duncan McAlester <duncan@rocketcom.com>
2019-10-03 13:23:16 -0700

    Revised masthead to better match existing site Minor refactor of HTML/CSS for slightly more streamlined markup/styling

--- packages/astro-uxds/js/navigation.js
+++ packages/astro-uxds/js/navigation.js
@@ -2,20 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const storage = typeof Storage !== undefined;
   const nav = document.getElementById('navigation');
 
-  nav.scrollTop = (storage && sessionStorage.getItem('navScrollPos')) || 0;
+  // nav.scrollTop = (storage && sessionStorage.getItem('navScrollPos')) || 0;
 
   // Listen for clicks isolated to the navigation element
   nav.addEventListener('click', (e) => {
     // reference clicked element
-    const selectedNavItem = e.target.parentElement;
+    const clickedNavItem = e.target.parentElement;
+    const selectedNavItem = nav.querySelector('[aria-selected="true"]');
+    const openedNavItems = nav.querySelectorAll('[aria-expanded="true"]');
 
-    console.log(storage);
+    selectedNavItem ? selectedNavItem.setAttribute('aria-selected', '') : '';
 
-    // toggle expanded/collapsed aria state to hide show element
-    if (selectedNavItem.getAttribute('aria-expanded')) {
-      selectedNavItem.setAttribute(
+    // make the tree behave like an accordion to mimic the existing site
+    // TODO: consider making this behave like a tree element
+    if (clickedNavItem.getAttribute('aria-expanded')) {
+      openedNavItems.forEach((item) => {
+        if (item !== clickedNavItem) {
+          item.setAttribute('aria-expanded', false);
+        }
+      });
+
+      // toggle the clicked on item expanded or callapsed, based on current state
+      clickedNavItem.setAttribute(
         'aria-expanded',
-        selectedNavItem.getAttribute('aria-expanded') === 'false' ? 'true' : 'false'
+        clickedNavItem.getAttribute('aria-expanded') === 'false' ? 'true' : 'false'
       );
     } else {
       if (storage) {
