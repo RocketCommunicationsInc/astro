04b7ac7935203ea101612c25a76c7a8af004589c
Duncan McAlester <duncan@rocketcom.com>
2019-10-10 10:18:46 -0700

    CSS changes to bring simplified templates closer to existing site

--- packages/astro-uxds/.eleventy.js
+++ packages/astro-uxds/.eleventy.js
@@ -16,12 +16,20 @@ module.exports = function(eleventyConfig) {
       figcaption: true,
     })
     .use(markdownItContainer, 'note')
-    .use(markdownItContainer, 'caution');
+    .use(markdownItContainer, 'caution')
+    .use(markdownItContainer, 'dos-and-donts');
   eleventyConfig.setLibrary('md', markdownLib);
+
+  /* Removes the h1 element from components to enabled inserting live sample */
+  eleventyConfig.addNunjucksFilter('removeHeader', function(value) {
+    value.val = value.val.replace(/<h1>(.*)<\/h1>/, '');
+    return value;
+  });
 
   // Manuall move static content
   eleventyConfig.addPassthroughCopy({ img: 'img/_site' });
   eleventyConfig.addPassthroughCopy({ '_content/img': 'img' });
+  eleventyConfig.addPassthroughCopy({ '_content/components/button/img': 'components/img' });
   eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('fonts');
