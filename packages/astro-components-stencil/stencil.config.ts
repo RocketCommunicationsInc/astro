3b08d1149b99f5f3385995abf0296c96e41f96a9
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-04-23 16:31:37 -0400

    feat: global styles

--- packages/astro-components-stencil/stencil.config.ts
+++ packages/astro-components-stencil/stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'astro-web-components',
+  globalStyle: 'src/global/global.css',
   outputTargets: [
     {
       type: 'dist',
