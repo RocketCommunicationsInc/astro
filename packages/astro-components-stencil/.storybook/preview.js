ed0779e9820be28dc8476c6ff58431089484dd4e
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-04-23 16:05:07 -0400

    storybook init

---
+++ packages/astro-components-stencil/.storybook/preview.js
@@ -0,0 +1,13 @@
+import {defineCustomElements} from '../dist/esm/loader';
+
+defineCustomElements();
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
