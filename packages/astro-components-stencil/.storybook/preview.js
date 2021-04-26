5a5f37d7206c271c72ffa831a109d35ecf4d39bc
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-04-26 16:58:46 -0400

    load storybook from stencil's dist dir

--- packages/astro-components-stencil/.storybook/preview.js
+++ packages/astro-components-stencil/.storybook/preview.js
@@ -1,7 +1,3 @@
-import {defineCustomElements} from '../dist/esm/loader';
-
-defineCustomElements();
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   layout: 'fullscreen',
