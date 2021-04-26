4f690584aa9c3b5cc97a54175f50e97e6ea6fa25
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-04-26 16:01:00 -0400

    remove padding from storybook stories

--- packages/astro-components-stencil/.storybook/preview.js
+++ packages/astro-components-stencil/.storybook/preview.js
@@ -4,6 +4,7 @@ defineCustomElements();
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: 'fullscreen',
   controls: {
     matchers: {
       color: /(background|color)$/i,
