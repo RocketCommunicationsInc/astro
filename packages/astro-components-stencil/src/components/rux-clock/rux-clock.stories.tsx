53c55ebb6291487dc88d56b0408e77c1af9883bd
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-04-26 13:20:10 -0400

    feat: add rux-clock storybook story

--- packages/astro-components-stencil/src/components/rux-clock/rux-clock.stories.tsx
+++ packages/astro-components-stencil/src/components/rux-clock/rux-clock.stories.tsx
@@ -1,11 +1,10 @@
 import { html, render } from 'lit-html';
 import { boolean, select, date, withKnobs } from '@storybook/addon-knobs';
-
 //@ts-ignore
 import readme from './readme.md';
 
 export default {
-  title: 'Components/Clock',
+  title: 'Components|Clock',
   decorators: [withKnobs],
 };
 
@@ -57,11 +56,6 @@ Clock.story = {
 };
 
 export const ClockWithAosLos = () => {
-  function dateWrapper(name, defaultValue) {
-    const stringTimestamp = date(name, defaultValue);
-    return new Date(stringTimestamp);
-  }
-
   const timezones = {
     Guam: 'Pacific/Guam',
     Hawaii: 'Pacific/Honolulu',
@@ -76,8 +70,9 @@ export const ClockWithAosLos = () => {
   };
 
   const timezoneKnob = select('Timezone', timezones, 'UTC');
-  const aosKnob = dateWrapper('AOS', new Date(1557503698781));
-  const losKnob = dateWrapper('LOS', new Date('2019-05-10T16:21:12.000Z'));
+  const aosKnob = date('AOS', new Date());
+  const losKnob = date('LOS', new Date());
+  const losAsISO = new Date(losKnob).toISOString();
   const hideTimezoneKnob = boolean('Hide Timezone', false);
   const hideDateKnob = boolean('Hide DOY', false);
   const smallKnob = boolean('Small Version', false);
@@ -85,8 +80,8 @@ export const ClockWithAosLos = () => {
     <div style="padding: 10%; display: flex; justify-content: center;">
       <rux-clock
         .timezone="${timezoneKnob}"
-        aos="${aosKnob.toString()}"
-        los="${losKnob.toString()}"
+        aos="${aosKnob}"
+        los="${losAsISO}"
         ?hideTimezone="${hideTimezoneKnob}"
         ?hideDate="${hideDateKnob}"
         ?small="${smallKnob}"
