fcd3d65cafb7f58917da5b88f407e7a03956c88d
Joel Perez <joel.perez@rocketcom.com>
2021-05-14 11:21:30 -0600

    added e2e and unit tests

--- packages/astro-components-stencil/src/components/rux-status/test/rux-status.spec.tsx
+++ packages/astro-components-stencil/src/components/rux-status/test/rux-status.spec.tsx
@@ -1,17 +1,17 @@
-// import { newSpecPage } from '@stencil/core/testing';
-// import { RuxStatus } from '../rux-status';
+import { newSpecPage } from '@stencil/core/testing';
+import { RuxStatus } from '../rux-status';
 
-// describe('rux-status', () => {
-//   it('renders', async () => {
-//     const page = await newSpecPage({
-//       components: [RuxStatus],
-//       html: `<rux-status></rux-status>`,
-//     });
-//     expect(page.root).toEqualHtml(`
-//       <rux-status>
-//         <mock:shadow-root>
-//         </mock:shadow-root>
-//       </rux-status>
-//     `);
-//   });
-// });
+describe('rux-status', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [RuxStatus],
+      html: `<rux-status></rux-status>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <rux-status>
+        <mock:shadow-root>
+        </mock:shadow-root>
+      </rux-status>
+    `);
+  });
+});
