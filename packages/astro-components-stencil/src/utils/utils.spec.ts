56494da510642d39fca14f0afad3e01c1d7e3581
Joel Perez <joel.perez@rocketcom.com>
2021-05-26 15:37:58 -0600

    moved collapseNotifcation function to common utilities

--- packages/astro-components-stencil/src/utils/utils.spec.ts
+++ packages/astro-components-stencil/src/utils/utils.spec.ts
@@ -1,4 +1,4 @@
-import { format } from './utils';
+import { format, collapseNotifications } from './utils';
 
 describe('format', () => {
   it('returns empty string for no names defined', () => {
@@ -17,3 +17,25 @@ describe('format', () => {
     expect(format('Joseph', 'Quincy', 'Publique')).toEqual('Joseph Quincy Publique');
   });
 });
+
+describe('collapses notifications', () => {
+  it('does not collapse 3 digit numbers', () => {
+    expect(collapseNotification(198)).toBe('198')
+  })
+
+  it('properly collapses 6-digit numbers', () => {
+    expect(collapseNotification(102894)).toBe('102K')
+  })
+
+  it('properly collapses 8-digit numbers', () => {
+    expect(collapseNotification(25684103)).toBe('25.7M')
+  })
+
+  it('properly collapses 10-digit numbers', () => {
+    expect(collapseNotification(3038953951)).toBe('3.0B')
+  })
+
+  it('accounts for extremely large numbers', () => {
+    expect(collapseNotification(20923509825234)).toBe('∞')
+  })
+})
--- packages/astro-components-stencil/src/utils/utils.ts
+++ packages/astro-components-stencil/src/utils/utils.ts
@@ -1,3 +1,31 @@
 export function format(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
+
+export function collapseNotifications(value: number) {
+  const n = Math.floor(value);
+
+  // don't show any values less than 0
+  if (n <= 0) return null;
+
+  // get the place value
+  const thousand = Math.floor((n / 1000) % 1000); // only return a whole number
+  const million = (n / 1000000) % 1000000; // return a decimal value for numbers like 1.2m
+  const billion = (n / 1000000000) % 1000000000; // return a decimal value for numbers like 1.2b
+  const trillion = (n / 1000000000000) % 1000000000000; // trillion is just to offer an overflow instance
+
+  // set the display to its original state
+  let _shorthand = n.toString();
+
+  if (trillion >= 1) {
+    _shorthand = '∞';
+  } else if (billion >= 1) {
+    _shorthand = `${billion.toFixed(1).toString()}B`;
+  } else if (million >= 1) {
+    _shorthand = `${million.toFixed(1).toString()}M`;
+  } else if (thousand >= 1) {
+    _shorthand = `${thousand}K`;
+  }
+
+  return _shorthand;
+}
