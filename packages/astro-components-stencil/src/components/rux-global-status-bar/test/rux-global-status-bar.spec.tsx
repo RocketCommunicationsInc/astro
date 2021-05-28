ea702c50167e13df593732b589a921c18d8e5f84
NicDominguez <nic.dominguez@rocketcom.com>
2021-05-28 09:20:43 -0700

    test: writes unit test for proper rendering

--- packages/astro-components-stencil/src/components/rux-global-status-bar/test/rux-global-status-bar.spec.tsx
+++ packages/astro-components-stencil/src/components/rux-global-status-bar/test/rux-global-status-bar.spec.tsx
@@ -10,8 +10,65 @@ describe('rux-global-status-bar', () => {
     expect(page.root).toEqualHtml(`
       <rux-global-status-bar>
         <mock:shadow-root>
-          <slot></slot>
+          <header>
+            <slot name="left-side"></slot>
+            <div class="app-meta" hidden>
+              <h1><span class="app-version"></span></h1>
+            </div>
+            <slot></slot>
+            <slot name="right-side"></slot>
+          </header>
         </mock:shadow-root>
+      </rux-global-status-bar>
+    `);
+  });
+
+  it('renders with app meta', async () => {
+    const page = await newSpecPage({
+      components: [RuxGlobalStatusBar],
+      html: `<rux-global-status-bar appname="Test App Name" version="test v1.0"></rux-global-status-bar>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <rux-global-status-bar appname="Test App Name" version="test v1.0">
+        <mock:shadow-root>
+          <header>
+            <slot name="left-side"></slot>
+            <div class="app-meta">
+              <h1>Test App Name<span class="app-version">test v1.0</span></h1>
+            </div>
+            <slot></slot>
+            <slot name="right-side"></slot>
+          </header>
+        </mock:shadow-root>
+      </rux-global-status-bar>
+    `);
+  });
+
+  it('renders with app meta and slotted content', async () => {
+    const page = await newSpecPage({
+      components: [RuxGlobalStatusBar],
+      html:
+        `<rux-global-status-bar appname="Test App Name" version="test v1.0">
+          <i slot="left-side">Menu Icon</i>
+          <div>Tab links<div>
+          <button slot="right-side">Master on/off</button>
+        </rux-global-status-bar>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <rux-global-status-bar appname="Test App Name" version="test v1.0">
+        <mock:shadow-root>
+          <header>
+            <slot name="left-side"></slot>
+            <div class="app-meta">
+              <h1>Test App Name<span class="app-version">test v1.0</span></h1>
+            </div>
+            <slot></slot>
+            <slot name="right-side"></slot>
+          </header>
+        </mock:shadow-root>
+        <i slot="left-side">Menu Icon</i>
+        <div>Tab links<div>
+        <button slot="right-side">Master on/off</button>
       </rux-global-status-bar>
     `);
   });
