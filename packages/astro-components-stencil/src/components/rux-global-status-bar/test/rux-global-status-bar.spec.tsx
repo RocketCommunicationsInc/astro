db0bf697aa58f74a4f5b2ad229c0cd85bc5e2a01
NicDominguez <nic.dominguez@rocketcom.com>
2021-06-03 09:01:11 -0700

    fix: changes attributes to kebab case and updates tests for recent compoennt changes

--- packages/astro-components-stencil/src/components/rux-global-status-bar/test/rux-global-status-bar.spec.tsx
+++ packages/astro-components-stencil/src/components/rux-global-status-bar/test/rux-global-status-bar.spec.tsx
@@ -26,16 +26,27 @@ describe('rux-global-status-bar', () => {
   it('renders with app meta', async () => {
     const page = await newSpecPage({
       components: [RuxGlobalStatusBar],
-      html: `<rux-global-status-bar appname="Test App Name" version="test v1.0"></rux-global-status-bar>`,
+      html: `<rux-global-status-bar app-domain="GRM" app-name="Test App Name" app-version="test v1.0"></rux-global-status-bar>`,
     });
     expect(page.root).toEqualHtml(`
-      <rux-global-status-bar appname="Test App Name" version="test v1.0">
+      <rux-global-status-bar app-domain="GRM" app-name="Test App Name" app-version="test v1.0" menu-icon="apps">
         <mock:shadow-root>
           <header>
-            <slot name="left-side"></slot>
-            <div class="app-meta">
-              <h1>Test App Name<span class="app-version">test v1.0</span></h1>
-            </div>
+            <slot name="left-side">
+              <rux-icon icon="apps" size="small"></rux-icon>
+            </slot>
+            <slot name="app-meta">
+              <div class="app-meta">
+                <div class="app-info-wrapper">
+                  <h1 class="app-domain">GRM</h1>
+                  <h1 class="app-name">TEST APP NAME
+                    <span class="app-version">test v1.0</span>
+                  </h1>
+                </div>
+                <div class="temp-app-state">App state</div>
+                <div class="temp-user-name">User Name</div>
+              </div>
+            </slot>
             <slot></slot>
             <slot name="right-side"></slot>
           </header>
@@ -47,26 +58,35 @@ describe('rux-global-status-bar', () => {
   it('renders with app meta and slotted content', async () => {
     const page = await newSpecPage({
       components: [RuxGlobalStatusBar],
-      html:
-        `<rux-global-status-bar appname="Test App Name" version="test v1.0">
-          <i slot="left-side">Menu Icon</i>
-          <div>Tab links<div>
-          <button slot="right-side">Master on/off</button>
-        </rux-global-status-bar>`,
+      html: `<rux-global-status-bar app-domain="GRM" app-name="Test App Name" app-version="test v1.0">
+                <div>Tab links<div>
+                <button slot="right-side">Master on/off</button>
+            </rux-global-status-bar>`,
     });
+    page.waitForChanges()
     expect(page.root).toEqualHtml(`
-      <rux-global-status-bar appname="Test App Name" version="test v1.0">
+      <rux-global-status-bar app-domain="GRM" app-name="Test App Name" app-version="test v1.0" menu-icon="apps">
         <mock:shadow-root>
           <header>
-            <slot name="left-side"></slot>
-            <div class="app-meta">
-              <h1>Test App Name<span class="app-version">test v1.0</span></h1>
-            </div>
+            <slot name="left-side">
+              <rux-icon icon="apps" size="small"></rux-icon>
+            </slot>
+            <slot name="app-meta">
+              <div class="app-meta">
+                <div class="app-info-wrapper">
+                  <h1 class="app-domain">GRM</h1>
+                  <h1 class="app-name">TEST APP NAME
+                    <span class="app-version">test v1.0</span>
+                  </h1>
+                </div>
+                <div class="temp-app-state">App state</div>
+                <div class="temp-user-name">User Name</div>
+              </div>
+            </slot>
             <slot></slot>
             <slot name="right-side"></slot>
           </header>
         </mock:shadow-root>
-        <i slot="left-side">Menu Icon</i>
         <div>Tab links<div>
         <button slot="right-side">Master on/off</button>
       </rux-global-status-bar>
