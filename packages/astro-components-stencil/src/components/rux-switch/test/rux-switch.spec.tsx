a8219fcd539026ac96e719150e4902af2cc2b615
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-06-16 15:24:21 -0400

    Merge pull request #36 from RocketCommunicationsInc/fix-switch
    Fix switch test

--- packages/astro-components-stencil/src/components/rux-switch/test/rux-switch.spec.tsx
+++ packages/astro-components-stencil/src/components/rux-switch/test/rux-switch.spec.tsx
@@ -8,15 +8,15 @@ describe('rux-switch', () => {
             html: `<rux-switch></rux-switch>`,
         })
         expect(page.root).toEqualHtml(`
-      <rux-switch>
-      <mock:shadow-root>
-      <div class="rux-switch">
-        <input class="rux-switch__input" id="rux-switch-0" type="checkbox">
-        <label class="rux-switch__button" htmlfor="rux-switch-0"></label>
-      </div>
-      </mock:shadow-root>
-      </rux-switch>
-    `)
+          <rux-switch aria-checked="false" role="switch">
+            <mock:shadow-root>
+              <div class="rux-switch">
+                <input aria-checked="false" class="rux-switch__input" id="rux-switch-0" role="switch" type="checkbox">
+                <label class="rux-switch__button" htmlfor="rux-switch-0"></label>
+              </div>
+              </mock:shadow-root>
+          </rux-switch>
+        `)
     })
 
     it('auto increments its own unique id', async () => {
