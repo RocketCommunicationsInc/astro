fe7b42070135eaa29bd391a42a003b96415caf5c
Joel Perez <joel.perez@rocketcom.com>
2021-06-28 16:17:33 -0600

    pop up menu e2e written

--- packages/astro-components-stencil/src/components/rux-pop-up-menu/test/rux-pop-up-menu.e2e.ts
+++ packages/astro-components-stencil/src/components/rux-pop-up-menu/test/rux-pop-up-menu.e2e.ts
@@ -7,5 +7,28 @@ describe('rux-pop-up-menu', () => {
 
     const element = await page.find('rux-pop-up-menu');
     expect(element).toHaveClass('hydrated');
-  });
+  })
+  it('opens menu when clicking on target element', async () => {
+    const page = await newE2EPage()
+    await page.setContent(
+      `<button aria-controls="pop-up-menu">C Button</button>
+      <rux-pop-up-menu id="pop-up-menu" open>
+        <rux-menu-item>Item 1</rux-menu-item>
+        <rux-menu-item-divider></rux-menu-item-divider>
+        <rux-menu-item value="2"
+            >Item 2 with an exceedingly long title that overruns the
+            width</rux-menu-item
+        >
+        <rux-menu-item disabled>Item 3 is disabled</rux-menu-item>
+      </rux-pop-up-menu>
+      `
+    )
+    const button = await page.find('button')
+    const menu = await page.find('rux-pop-up-menu')
+    // Menu is added in closed state
+    expect(menu).not.toHaveAttribute('open')
+    await button.click()
+    // Test opening menu
+    expect(menu).toHaveAttribute('open')
+  })
 });
