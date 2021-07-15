76d6a7f5771c444a496d5bc530d15099ae9abc97
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-07-15 12:21:18 -0400

    Merge pull request #63 from RocketCommunicationsInc/ASTRO-1553-tree-keyboard-controls
    Astro 1553 tree keyboard controls

--- packages/astro-components-stencil/src/components/rux-tree/test/rux-tree.e2e.ts
+++ packages/astro-components-stencil/src/components/rux-tree/test/rux-tree.e2e.ts
@@ -1,11 +1,20 @@
-import { newE2EPage } from '@stencil/core/testing'
+import { E2EPage, newE2EPage } from '@stencil/core/testing'
 
 describe('rux-tree', () => {
-    it('renders', async () => {
-        const page = await newE2EPage()
-        await page.setContent('<rux-tree></rux-tree>')
+    let page!: E2EPage
 
-        const element = await page.find('rux-tree')
-        expect(element).toHaveClass('hydrated')
+    beforeEach(async () => {
+        page = await newE2EPage()
+        await page.setContent(`<rux-tree>
+        <rux-tree-node id="first">Level 1</rux-tree-node>
+        <rux-tree-node>Level 2</rux-tree-node>
+      </rux-tree>`)
+    })
+    it('focuses first node', async () => {
+        await page.keyboard.down('Tab')
+        const activeElement = await page.evaluate(
+            () => document.activeElement!.id
+        )
+        expect(activeElement).toBe('first')
     })
 })
