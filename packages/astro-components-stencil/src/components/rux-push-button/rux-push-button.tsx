8ffbc42f41fa4ee12e90e3a315655ede514ce245
NicDominguez <nic.dominguez@rocketcom.com>
2021-07-22 14:11:29 -0700

    feat: ports LitElement props and html

--- packages/astro-components-stencil/src/components/rux-push-button/rux-push-button.tsx
+++ packages/astro-components-stencil/src/components/rux-push-button/rux-push-button.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core'
+import { Prop, Component, Host, h } from '@stencil/core'
 
 @Component({
     tag: 'rux-push-button',
@@ -6,10 +6,24 @@ import { Component, Host, h } from '@stencil/core'
     shadow: true,
 })
 export class RuxPushButton {
+    @Prop() disabled: boolean = false
+    @Prop() checkedLabel: string = 'Enabled'
+    @Prop() uncheckedLabel: string = 'Disabled'
+    @Prop({ reflect: true }) checked: boolean = false
+
     render() {
         return (
             <Host>
-                <slot></slot>
+                <input
+                    class="rux-push-button__input"
+                    id="ruxSwitch"
+                    type="checkbox"
+                    disabled={this.disabled}
+                    checked={this.checked}
+                />
+                <label class="rux-push-button__button" htmlFor="ruxSwitch">
+                    <slot></slot>
+                </label>
             </Host>
         )
     }
