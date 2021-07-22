02d4af0ce9f6e67fd4c91dafedcc961101ef5c3d
NicDominguez <nic.dominguez@rocketcom.com>
2021-07-22 16:06:43 -0700

    feat: adds support for checked/unchecked labels

--- packages/astro-components-stencil/src/components/rux-push-button/rux-push-button.tsx
+++ packages/astro-components-stencil/src/components/rux-push-button/rux-push-button.tsx
@@ -1,4 +1,4 @@
-import { Prop, Component, Host, h } from '@stencil/core'
+import { Prop, Component, Host, h, State, Listen } from '@stencil/core'
 
 @Component({
     tag: 'rux-push-button',
@@ -9,20 +9,34 @@ export class RuxPushButton {
     @Prop() disabled: boolean = false
     @Prop() checkedLabel: string = 'Enabled'
     @Prop() uncheckedLabel: string = 'Disabled'
-    @Prop({ reflect: true }) checked: boolean = false
+    @Prop({ reflect: true, mutable: true }) checked: boolean = false
+
+    @State() label: string = this.uncheckedLabel
+
+    @Listen('click')
+    handleClick(e: Event) {
+        e.preventDefault()
+        this.checked = !this.checked
+        if (this.checked) {
+            this.label = this.checkedLabel
+        } else {
+            this.label = this.uncheckedLabel
+        }
+    }
 
     render() {
+        const { disabled, checked, label } = this
         return (
             <Host>
                 <input
                     class="rux-push-button__input"
                     id="ruxSwitch"
                     type="checkbox"
-                    disabled={this.disabled}
-                    checked={this.checked}
+                    disabled={disabled}
+                    checked={checked}
                 />
                 <label class="rux-push-button__button" htmlFor="ruxSwitch">
-                    <slot></slot>
+                    <slot>{label}</slot>
                 </label>
             </Host>
         )
