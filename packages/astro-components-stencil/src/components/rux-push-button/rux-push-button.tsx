14364abd660606592afaa15b33a1ce4402e2d9af
NicDominguez <nic.dominguez@rocketcom.com>
2021-07-22 13:57:47 -0700

    style: ports LitElement styles

--- packages/astro-components-stencil/src/components/rux-push-button/rux-push-button.tsx
+++ packages/astro-components-stencil/src/components/rux-push-button/rux-push-button.tsx
@@ -2,7 +2,7 @@ import { Component, Host, h } from '@stencil/core'
 
 @Component({
     tag: 'rux-push-button',
-    styleUrl: 'rux-push-button.css',
+    styleUrl: 'rux-push-button.scss',
     shadow: true,
 })
 export class RuxPushButton {
