27bc409a418df57e84028f9ceb74f137023d467f
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-08-10 11:22:31 -0400

    Astro 1674 checkbox form (#109)
    * test(rux-checkbox): submits value with form
    
    * test(rux-checkbox): fix test
    
    * test(rux-checkbox): additional disabled assertions
    
    * test(rux-checkbox): move test
    
    * test(rux-checkbox): rm async
    
    * fix: rm async
    
    * ci: temp rm cypress runner
    
    * test(rux-checkbox): test checked / unchecked submit

---
+++ packages/astro-components-stencil/src/components/rux-checkbox/test/form-checkbox.e2e.js
@@ -0,0 +1,59 @@
+describe('Checkbox with Form', () => {
+    beforeEach(() => {
+        cy.visit('localhost:4444/tests/pages/form-checkbox.html')
+    })
+    it('submits the correct select value when using a form', () => {
+        cy.get('#ruxCheckbox').shadow().find('input').click({ force: true })
+        cy.get('#nativeCheckbox').click()
+        cy.get('#form').submit()
+        cy.get('#log').contains('ruxCheckbox:foo')
+        cy.get('#log').contains('nativeCheckbox:foo')
+    })
+
+    it('defaults to unchecked', () => {
+        cy.get('#ruxCheckbox')
+            .shadow()
+            .find('input')
+            .should('not.be', 'checked')
+        cy.get('#nativeCheckbox').should('not.be', 'checked')
+    })
+
+    it('does not submit any value if not checked', () => {
+        cy.get('#form').submit()
+        cy.get('#log').should('not.contain', 'ruxCheckbox')
+        cy.get('#log').should('not.contain', 'nativeCheckbox')
+    })
+
+    it('submits a value of "on" if no value is provided', () => {
+        cy.get('#ruxCheckbox2').shadow().find('input').click({ force: true })
+        cy.get('#nativeCheckbox2').click()
+        cy.get('#form-no-value').submit()
+        cy.get('#log').contains('ruxCheckbox:on')
+        cy.get('#log').contains('nativeCheckbox:on')
+    })
+
+    it('does not allow input if disabled', () => {
+        cy.get('#ruxCheckboxDisabled')
+            .shadow()
+            .find('input')
+            .should('be.disabled')
+        cy.get('#ruxCheckboxDisabled')
+            .shadow()
+            .find('input')
+            .click({ force: true })
+        cy.get('#ruxCheckboxDisabled')
+            .shadow()
+            .find('input')
+            .should('not.be', 'checked')
+        cy.get('#nativeCheckbox2').click()
+        cy.get('#form').submit()
+        cy.get('#log').should('not.contain', 'ruxCheckboxDisabled')
+    })
+
+    it('does not submit a value if checked and then unchecked', () => {
+        cy.get('#ruxCheckbox').shadow().find('input').click({ force: true })
+        cy.get('#ruxCheckbox').shadow().find('input').click({ force: true })
+        cy.get('#form').submit()
+        cy.get('#log').should('not.contain', 'ruxCheckbox')
+    })
+})
