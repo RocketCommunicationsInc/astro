510a78c54d3068a73f8f6ba7fb0008355a32fa7b
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-08-11 17:08:01 -0400

    Astro 1629 form button (#121)
    * feat(rux-button): add type for native form support

--- packages/astro-components-stencil/src/components/rux-button/rux-button.tsx
+++ packages/astro-components-stencil/src/components/rux-button/rux-button.tsx
@@ -1,4 +1,5 @@
-import { Prop, Component, h } from '@stencil/core'
+import { Prop, Element, Component, h } from '@stencil/core'
+import { hasShadowDom } from '../../utils/utils'
 
 @Component({
     tag: 'rux-button',
@@ -6,6 +7,7 @@ import { Prop, Component, h } from '@stencil/core'
     shadow: true,
 })
 export class RuxButton {
+    @Element() el!: HTMLRuxButtonElement
     /**
      * For a [button style guide, see the Button section in Astro UXDS Guidelines](https://astrouxds.com/components/button)
      * Displays an Astro icon matching this string. For a [full list of available icons,
@@ -37,11 +39,41 @@ export class RuxButton {
      */
     @Prop({ reflect: true }) size?: 'small' | 'medium' | 'large'
 
+    /**
+     * The button type. Use 'submit' to submit native form data.
+     */
+    @Prop() type: 'submit' | 'button' = 'button'
+
+    /**
+     * Creates and appends a native <button> if used within a form
+     * so that it can trigger the submit event.
+     *
+     * Shadow DOM limitation prevents <form> from working correctly.
+     * https://github.com/ionic-team/ionic-framework/blob/main/core/src/components/button/button.tsx
+     * @param ev
+     */
+    private handleClick = (ev: Event) => {
+        if (hasShadowDom(this.el)) {
+            const form = this.el.closest('form')
+            if (form) {
+                ev.preventDefault()
+
+                const fakeButton = document.createElement('button')
+                fakeButton.type = this.type
+                fakeButton.style.display = 'none'
+                form.appendChild(fakeButton)
+                fakeButton.click()
+                fakeButton.remove()
+            }
+        }
+    }
+
     render() {
         const { size, iconOnly, secondary, disabled, icon } = this
         return (
             <button
                 type="button"
+                onClick={this.handleClick}
                 class={{
                     'rux-button': true,
                     'rux-button--secondary': secondary,
--- packages/astro-components-stencil/src/components/rux-button/test/rux-button.e2e.js
+++ packages/astro-components-stencil/src/components/rux-button/test/rux-button.e2e.js
@@ -6,3 +6,13 @@ describe('Button', () => {
         cy.get('rux-button').should('have.class', 'hydrated')
     })
 })
+
+describe('Button with Form', () => {
+    beforeEach(() => {
+        cy.visit('localhost:4444/tests/pages/form-button.html')
+    })
+    it('submits the correct select value when using a form', () => {
+        cy.get('rux-button').shadow().find('button').click({ force: true })
+        cy.get('#log').contains('myname:mark')
+    })
+})
