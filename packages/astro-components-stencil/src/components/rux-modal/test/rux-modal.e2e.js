280a8c3a2da7fc5127f1ca602146fb478b6d4275
Nic Dominguez <NicDominguez@users.noreply.github.com>
2021-08-12 12:00:05 -0700

    Merge pull request #126 from RocketCommunicationsInc/ASTRO-1688-dialog-tests
    ASTRO-1688 Modal tests

--- packages/astro-components-stencil/src/components/rux-modal/test/rux-modal.e2e.js
+++ packages/astro-components-stencil/src/components/rux-modal/test/rux-modal.e2e.js
@@ -5,4 +5,66 @@ describe('Modal', () => {
     it('renders', () => {
         cy.get('rux-modal').should('have.class', 'hydrated')
     })
+    it('should close and open the dialog', () => {
+        cy.get('rux-modal').then(($modal) => {
+            $modal[0].setAttribute('open', false)
+        })
+        cy.get('rux-modal').then(($modal) => {
+            $modal[0].setAttribute('open', true)
+        })
+        cy.get('rux-modal').shadow().find('.rux-modal__wrapper').should('exist')
+    })
+
+    it('should display new modal title in the dialog when changed', () => {
+        cy.get('rux-modal').then(($modal) => {
+            $modal[0].setAttribute('modal-title', 'This is a test title')
+        })
+        cy.get('rux-modal').shadow().find('h1').contains('This is a test title')
+    })
+
+    it('should display new modal message in the dialog when changed', () => {
+        cy.get('rux-modal').then(($modal) => {
+            $modal[0].setAttribute('modal-message', 'This is a test message')
+        })
+        cy.get('rux-modal')
+            .shadow()
+            .find('.rux-modal__message')
+            .contains('This is a test message')
+    })
+
+    it('should display new confirmation text in the dialog when changed', () => {
+        cy.get('rux-modal').then(($modal) => {
+            $modal[0].setAttribute('confirm-text', 'Test Confirm Text')
+        })
+        cy.get('rux-modal')
+            .shadow()
+            .find('[data-value="true"]')
+            .contains('Test Confirm Text')
+    })
+
+    it('should display new deny text in the dialog when changed', () => {
+        cy.get('rux-modal').then(($modal) => {
+            $modal[0].setAttribute('deny-text', 'Test Deny Text')
+        })
+        cy.get('rux-modal')
+            .shadow()
+            .find('[data-value="false"]')
+            .contains('Test Deny Text')
+    })
+
+    it('close the modal when deny clicked', () => {
+        cy.get('rux-modal').shadow().find('[data-value="false"]').click()
+        cy.get('rux-modal')
+            .shadow()
+            .find('.rux-modal__wrapper')
+            .should('not.exist')
+    })
+
+    it('close the modal when confirm clicked', () => {
+        cy.get('rux-modal').shadow().find('[data-value="true"]').click()
+        cy.get('rux-modal')
+            .shadow()
+            .find('.rux-modal__wrapper')
+            .should('not.exist')
+    })
 })
