a418914cdf1990e38a05cb9d17b8d3980d81321d
NicDominguez <nic.dominguez@rocketcom.com>
2021-09-08 13:59:53 -0700

    build: configures sass

--- packages/ag-grid-theme/webpack.config.js
+++ packages/ag-grid-theme/webpack.config.js
@@ -1,5 +1,12 @@
+const path = require("path");
+
 module.exports = {
   mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
       {
