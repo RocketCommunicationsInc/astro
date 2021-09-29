0b75967d185e61ffbc66f9e0723170b4a55e9028
Micah Jones <micah.jones@rocketcom.com>
2021-09-29 10:49:00 -0600

    test(tree): render test passing

--- packages/astro-in-react/__tests__/RuxTree.spec.tsx
+++ packages/astro-in-react/__tests__/RuxTree.spec.tsx
@@ -7,45 +7,19 @@ test("use jsdom in this test file", () => {
   expect(element).not.toBeNull();
 });
 
-// import React from "react";
-// import { RuxTree, RuxTreeNode } from "../src";
-// import "@testing-library/jest-dom";
-// import {
-//   renderWithStrictMode,
-//   //   includeWebComponent,
-// } from "./common/commonFunctions";
-// // import { fireEvent } from "@testing-library/dom";
+import React from "react";
+import { RuxTree, RuxTreeNode } from "../src";
+import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
 
-// describe("RuxTree", () => {
-//   it("should be rendered by react", () => {
-//     const { container } = renderWithStrictMode(
-//       <RuxTree>
-//         <RuxTreeNode>
-//           Child
-//           <RuxTreeNode slot="node">Child</RuxTreeNode>
-//         </RuxTreeNode>
-//       </RuxTree>
-//     );
-//     const comp = container.getElementsByTagName("rux-tree-node")[0];
-//     expect(comp).toBeInTheDocument();
-//   });
-// });
-// // describe("createComponent - ref", () => {
-// //   test("should pass ref on to web component instance", () => {
-// //     const TreeRef: React.RefObject<any> = React.createRef();
-// //     const { webcomponent: ruxTree } = includeWebComponent<HTMLRuxTreeElement>(
-// //       renderWithStrictMode(<RuxTree ref={TreeRef}></RuxTree>)
-// //     );
-// //     expect(TreeRef.current).toEqual(ruxTree);
-// //   });
-// // });
-// // describe("createComponent - events", () => {
-// //   test("should set events on handler", () => {
-// //     const FakeOnClick = jest.fn((e) => e);
-// //     const { webcomponent } = includeWebComponent<HTMLRuxTreeElement>(
-// //       renderWithStrictMode(<RuxTree onClick={FakeOnClick}></RuxTree>)
-// //     );
-// //     fireEvent.click(webcomponent);
-// //     expect(FakeOnClick).toBeCalledTimes(1);
-// //   });
-// // });
+describe("RuxTree", () => {
+  it("should be rendered by react", () => {
+    const container = renderer.create(
+      <RuxTree>
+        <RuxTreeNode>Kid</RuxTreeNode>
+      </RuxTree>
+    );
+    let tree = container.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
