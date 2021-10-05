006e2849fe3f56fa56cf030fc6e1b7d6262fe83e
NicDominguez <nic.dominguez@rocketcom.com>
2021-10-05 14:11:21 -0700

    feat(svelte-starter): adds astro to starter

--- packages/starter-kits/svelte-starter/src/main.ts
+++ packages/starter-kits/svelte-starter/src/main.ts
@@ -1,10 +1,14 @@
 import App from './App.svelte';
 
+// Import Astro's base styles
+import "../node_modules/@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css";
+// Define the Astro Components
+import { defineCustomElements } from '@astrouxds/astro-web-components/dist/custom-elements';
+defineCustomElements();
+
+
 const app = new App({
 	target: document.body,
-	props: {
-		name: 'world'
-	}
 });
 
 export default app;
