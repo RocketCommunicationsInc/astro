0ca672bd12380fb2ff84566a80d5d7dc487f9021
Micah Jones <micah.jones@rocketcom.com>
2021-10-05 09:11:34 -0600

    feat(classification-markings): added unclassified to default SB args, removed text fallback

--- packages/web-components/src/components/rux-classification-marking/rux-classification-marking.tsx
+++ packages/web-components/src/components/rux-classification-marking/rux-classification-marking.tsx
@@ -69,9 +69,6 @@ export class RuxClassificationMarking {
         }
 
         const text = markings[this.type][this.classification]
-        if (!text) {
-            return 'Select a Classification Marking'
-        }
         return text
     }
 
