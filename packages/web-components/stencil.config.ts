49d3065e7facce675c03b81025cfb03718e0f384
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-10-12 10:46:46 -0400

    build: add react output target

--- packages/web-components/stencil.config.ts
+++ packages/web-components/stencil.config.ts
@@ -3,24 +3,24 @@ import { sass } from '@stencil/sass'
 import { svgOptimizerPlugin } from './src/utils/rollup-svg'
 import { angularValueAccessorBindings } from './wrapper-bindings/angular.bindings'
 import { angularOutputTarget } from '@stencil/angular-output-target'
-// import { reactOutputTarget } from '@stencil/react-output-target';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
     namespace: 'astro-web-components',
     globalStyle: 'src/global/global.scss',
     outputTargets: [
-        angularOutputTarget({
-            componentCorePackage: '@astrouxds/astro-web-components',
-            directivesProxyFile: '../angular/src/directives/proxies.ts',
-            directivesArrayFile: '../angular/src/directives/proxies-list.ts',
-            valueAccessorConfigs: angularValueAccessorBindings,
-        }),
-        // reactOutputTarget({
-        //   componentCorePackage: '@astrouxds/astro-web-components',
-        //   proxiesFile: '../astro-in-react/src/components.ts',
-        //   includePolyfills: true,
-        //   includeDefineCustomElements: true
+        // angularOutputTarget({
+        //     componentCorePackage: '@astrouxds/astro-web-components',
+        //     directivesProxyFile: '../angular/src/directives/proxies.ts',
+        //     directivesArrayFile: '../angular/src/directives/proxies-list.ts',
+        //     valueAccessorConfigs: angularValueAccessorBindings,
         // }),
+        reactOutputTarget({
+          componentCorePackage: '@astrouxds/astro-web-components',
+          proxiesFile: '../react/src/components.tsx',
+          includePolyfills: true,
+          includeDefineCustomElements: true
+        }),
         {
             type: 'dist',
             esmLoaderPath: '../loader',
