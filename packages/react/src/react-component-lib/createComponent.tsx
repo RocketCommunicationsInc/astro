f99d8a3c10052796b68495fb46f4522884f14ecd
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-10-12 10:51:02 -0400

    chore: run build

--- packages/react/src/react-component-lib/createComponent.tsx
+++ packages/react/src/react-component-lib/createComponent.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
 import {
   attachProps,
   createForwardRef,
   dashToPascalCase,
+  defineCustomElement,
   isCoveredByReact,
   mergeRefs,
-} from "./utils";
+} from './utils';
 
 export interface HTMLStencilElement extends HTMLElement {
   componentOnReady(): Promise<this>;
 }
 
-interface StencilReactInternalProps<ElementType>
-  extends React.HTMLAttributes<ElementType> {
+interface StencilReactInternalProps<ElementType> extends React.HTMLAttributes<ElementType> {
   forwardedRef: React.RefObject<ElementType>;
   ref?: React.Ref<any>;
 }
@@ -28,14 +28,14 @@ export const createReactComponent = <
   ReactComponentContext?: React.Context<ContextStateType>,
   manipulatePropsFunction?: (
     originalProps: StencilReactInternalProps<ElementType>,
-    propsToPass: any
-  ) => ExpandedPropsTypes
+    propsToPass: any,
+  ) => ExpandedPropsTypes,
+  customElement?: any,
 ) => {
-  const displayName = dashToPascalCase(tagName);
+  defineCustomElement(tagName, customElement);
 
-  const ReactComponent = class extends React.Component<
-    StencilReactInternalProps<ElementType>
-  > {
+  const displayName = dashToPascalCase(tagName);
+  const ReactComponent = class extends React.Component<StencilReactInternalProps<ElementType>> {
     componentEl!: ElementType;
 
     setComponentElRef = (element: ElementType) => {
@@ -55,19 +55,12 @@ export const createReactComponent = <
     }
 
     render() {
-      const {
-        children,
-        forwardedRef,
-        style,
-        className,
-        ref,
-        ...cProps
-      } = this.props;
+      const { children, forwardedRef, style, className, ref, ...cProps } = this.props;
 
       let propsToPass = Object.keys(cProps).reduce((acc, name) => {
-        if (name.indexOf("on") === 0 && name[2] === name[2].toUpperCase()) {
+        if (name.indexOf('on') === 0 && name[2] === name[2].toUpperCase()) {
           const eventName = name.substring(2).toLowerCase();
-          if (typeof document !== "undefined" && isCoveredByReact(eventName)) {
+          if (typeof document !== 'undefined' && isCoveredByReact(eventName)) {
             (acc as any)[name] = (cProps as any)[name];
           }
         } else {
@@ -80,10 +73,7 @@ export const createReactComponent = <
         propsToPass = manipulatePropsFunction(this.props, propsToPass);
       }
 
-      const newProps: Omit<
-        StencilReactInternalProps<ElementType>,
-        "forwardedRef"
-      > = {
+      const newProps: Omit<StencilReactInternalProps<ElementType>, 'forwardedRef'> = {
         ...propsToPass,
         ref: mergeRefs(forwardedRef, this.setComponentElRef),
         style,
--- packages/react/src/react-component-lib/utils/attachProps.ts
+++ packages/react/src/react-component-lib/utils/attachProps.ts
@@ -1,30 +1,26 @@
-import { camelToDashCase } from "./case";
+import { camelToDashCase } from './case';
 
-export const attachProps = (
-  node: HTMLElement,
-  newProps: any,
-  oldProps: any = {}
-) => {
+export const attachProps = (node: HTMLElement, newProps: any, oldProps: any = {}) => {
   // some test frameworks don't render DOM elements, so we test here to make sure we are dealing with DOM first
   if (node instanceof Element) {
     // add any classes in className to the class list
     const className = getClassName(node.classList, newProps, oldProps);
-    if (className !== "") {
+    if (className !== '') {
       node.className = className;
     }
 
     Object.keys(newProps).forEach((name) => {
       if (
-        name === "children" ||
-        name === "style" ||
-        name === "ref" ||
-        name === "class" ||
-        name === "className" ||
-        name === "forwardedRef"
+        name === 'children' ||
+        name === 'style' ||
+        name === 'ref' ||
+        name === 'class' ||
+        name === 'className' ||
+        name === 'forwardedRef'
       ) {
         return;
       }
-      if (name.indexOf("on") === 0 && name[2] === name[2].toUpperCase()) {
+      if (name.indexOf('on') === 0 && name[2] === name[2].toUpperCase()) {
         const eventName = name.substring(2);
         const eventNameLc = eventName[0].toLowerCase() + eventName.substring(1);
 
@@ -34,7 +30,7 @@ export const attachProps = (
       } else {
         (node as any)[name] = newProps[name];
         const propType = typeof newProps[name];
-        if (propType === "string") {
+        if (propType === 'string') {
           node.setAttribute(camelToDashCase(name), newProps[name]);
         }
       }
@@ -42,21 +38,13 @@ export const attachProps = (
   }
 };
 
-export const getClassName = (
-  classList: DOMTokenList,
-  newProps: any,
-  oldProps: any
-) => {
+export const getClassName = (classList: DOMTokenList, newProps: any, oldProps: any) => {
   const newClassProp: string = newProps.className || newProps.class;
   const oldClassProp: string = oldProps.className || oldProps.class;
   // map the classes to Maps for performance
   const currentClasses = arrayToMap(classList);
-  const incomingPropClasses = arrayToMap(
-    newClassProp ? newClassProp.split(" ") : []
-  );
-  const oldPropClasses = arrayToMap(
-    oldClassProp ? oldClassProp.split(" ") : []
-  );
+  const incomingPropClasses = arrayToMap(newClassProp ? newClassProp.split(' ') : []);
+  const oldPropClasses = arrayToMap(oldClassProp ? oldClassProp.split(' ') : []);
   const finalClassNames: string[] = [];
   // loop through each of the current classes on the component
   // to see if it should be a part of the classNames added
@@ -71,7 +59,7 @@ export const getClassName = (
     }
   });
   incomingPropClasses.forEach((s) => finalClassNames.push(s));
-  return finalClassNames.join(" ");
+  return finalClassNames.join(' ');
 };
 
 /**
@@ -79,16 +67,16 @@ export const getClassName = (
  * @license Modernizr 3.0.0pre (Custom Build) | MIT
  */
 export const isCoveredByReact = (eventNameSuffix: string) => {
-  if (typeof document === "undefined") {
+  if (typeof document === 'undefined') {
     return true;
   } else {
-    const eventName = "on" + eventNameSuffix;
+    const eventName = 'on' + eventNameSuffix;
     let isSupported = eventName in document;
 
     if (!isSupported) {
-      const element = document.createElement("div");
-      element.setAttribute(eventName, "return;");
-      isSupported = typeof (element as any)[eventName] === "function";
+      const element = document.createElement('div');
+      element.setAttribute(eventName, 'return;');
+      isSupported = typeof (element as any)[eventName] === 'function';
     }
 
     return isSupported;
@@ -96,9 +84,7 @@ export const isCoveredByReact = (eventNameSuffix: string) => {
 };
 
 export const syncEvent = (
-  node: Element & {
-    __events?: { [key: string]: ((e: Event) => any) | undefined };
-  },
+  node: Element & { __events?: { [key: string]: ((e: Event) => any) | undefined } },
   eventName: string,
   newEventHandler?: (e: Event) => any
 ) => {
