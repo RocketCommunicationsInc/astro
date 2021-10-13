73efdab2136ba584c8582dfdf8afef282071be36
Micah Jones <micah.jones@rocketcom.com>
2021-10-13 11:00:19 -0600

    starter-kits(svelte): removed components to make basic starter kit

--- packages/starter-kits/svelte-starter/src/App.svelte
+++ packages/starter-kits/svelte-starter/src/App.svelte
@@ -1,12 +1,16 @@
-<script>
-  import Header from "./components/Header.svelte";
-  import Main from "./components/Main.svelte";
-  import Footer from "./components/Footer.svelte";
-</script>
-
-<Header />
-<Main />
-<Footer />
-
+<script></script>
+<main>
+  <rux-global-status-bar appname="App">
+    <rux-clock aos="1557503698781" los="2019-05-10T16:21:12.000Z"></rux-clock>
+    </rux-global-status-bar>
+    <div>
+      <h1>Astro Svelte Starter Kit</h1>
+    </div>
+</main>
 <style>
+  div{
+    width: 40%;
+    margin: 50px auto 0 auto;
+    text-align: center;
+  }
 </style>
