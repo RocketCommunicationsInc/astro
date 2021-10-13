f2dc24f9779760f593b2800e0aedabc60381a1be
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-10-13 15:50:15 -0400

    * fix(angular): prevent events firing twice
    * fix(angular): prevent events firing twice
    
    * revert(angular): unused params
    
    * fix(angular): remove ionic ref

--- packages/angular/scripts/rollup.config.js
+++ packages/angular/scripts/rollup.config.js
@@ -1,24 +1,20 @@
-import resolve from "rollup-plugin-node-resolve";
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: "build/es2015/core.js",
+  input: 'build/es2015/core.js',
   output: {
-    file: "dist/fesm2015.js",
-    format: "es",
+    file: 'dist/fesm2015.js',
+    format: 'es',
   },
   external: (id) => {
     // inline @ionic/core deps
-    if (id === "@ionic/core") {
+    if (id === '@ionic/core') {
       return false;
     }
     // anything else is external
     // Windows: C:\xxxxxx\xxx
     const colonPosition = 1;
-    return !(
-      id.startsWith(".") ||
-      id.startsWith("/") ||
-      id.charAt(colonPosition) === ":"
-    );
+    return !(id.startsWith('.') || id.startsWith('/') || id.charAt(colonPosition) === ':');
   },
   plugins: [
     resolve({
--- packages/angular/scripts/rollup.config.legacy.js
+++ packages/angular/scripts/rollup.config.legacy.js
@@ -1,9 +1,9 @@
-import config from "./rollup.config";
+import config from './rollup.config';
 
 const newConfig = {
   ...config,
-  input: "build/es5/core.js",
+  input: 'build/es5/core.js',
 };
-newConfig.output.file = "dist/fesm5.js";
+newConfig.output.file = 'dist/fesm5.js';
 
 export { newConfig as default };
