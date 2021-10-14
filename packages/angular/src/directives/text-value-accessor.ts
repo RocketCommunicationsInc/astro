3c76efee965c5076e3077ccf23341000e9c2b0eb
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-10-14 12:31:21 -0400

    * feat(angular): value accessors for select, slider
    * feat(angular): value accessors for select, slider
    
    * chore(angular): run build
    
    * chore(angular): bump version

--- packages/angular/src/directives/text-value-accessor.ts
+++ packages/angular/src/directives/text-value-accessor.ts
@@ -5,11 +5,10 @@ import { ValueAccessor } from './value-accessor';
 
 @Directive({
   /* tslint:disable-next-line:directive-selector */
-  selector: 'rux-input, rux-textarea, rux-radio-group, rux-radio',
+  selector: 'rux-input, rux-textarea, rux-slider, rux-radio-group, rux-select',
   host: {
     '(ruxinput)': 'handleChangeEvent($event.target.value)',
-    '(ruxchange)': 'handleChangeEvent($event.target.value)',
-    '(ruxblur)': 'handleChangeEvent($event.target.value)'
+    '(ruxchange)': 'handleChangeEvent($event.target.value)'
   },
   providers: [
     {
--- packages/web-components/wrapper-bindings/angular.bindings.ts
+++ packages/web-components/wrapper-bindings/angular.bindings.ts
@@ -2,7 +2,7 @@ import { ValueAccessorConfig } from '@stencil/angular-output-target'
 
 export const angularValueAccessorBindings: ValueAccessorConfig[] = [
     {
-        elementSelectors: ['rux-input', 'rux-textarea'],
+        elementSelectors: ['rux-input', 'rux-textarea', 'rux-slider'],
         event: 'ruxinput',
         targetAttr: 'value',
         type: 'text',
@@ -14,15 +14,9 @@ export const angularValueAccessorBindings: ValueAccessorConfig[] = [
         type: 'boolean',
     },
     {
-        elementSelectors: ['rux-radio-group'],
+        elementSelectors: ['rux-radio-group', 'rux-select'],
         event: 'ruxchange',
         targetAttr: 'value',
         type: 'text',
-    },
-    {
-        elementSelectors: ['rux-radio'],
-        event: 'ruxblur',
-        targetAttr: 'value',
-        type: 'text',
-    },
+    }
 ]
