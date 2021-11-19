f271405e8478c7b3be8194780f2370321c5664ab
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-11-19 16:25:53 -0500

    test(notification): add test coverage for close

--- packages/web-components/src/components/rux-notification/test/rux-notification.e2e.js
+++ packages/web-components/src/components/rux-notification/test/rux-notification.e2e.js
@@ -6,22 +6,15 @@ describe('Notification', () => {
         cy.get('rux-notification').should('have.class', 'hydrated')
     })
 
-    //   it('closes after time is up', async () => {
-    //     const page = await newE2EPage()
-    //     await page.setContent(
-    //         '<rux-notification open close-after="3000" status="caution" message="testing time"></rux-notification>'
-    //     )
-    //     const el = await page.find('rux-notification')
-    //     //Wait for notification time limit to be up before checking open attr
-    //     setTimeout(() => expect(el).not.toHaveAttribute('open'), 3001)
-    // })
-    // it('does not close before time is up', async () => {
-    //     const page = await newE2EPage()
-    //     await page.setContent(
-    //         '<rux-notification open close-after="3000" status="caution" message="testing time"></rux-notification>'
-    //     )
-    //     const el = await page.find('rux-notification')
-    //     //Checking open attr right before time is up to make sure it doesn't close a ms too early
-    //     setTimeout(() => expect(el).toHaveAttribute('open'), 2999)
-    // })
+    it('closes when close icon is clicked', () => {
+        const notification = cy.get('rux-notification[data-test-id="default"]')
+        notification.should('have.attr', 'open')
+        const icon = cy
+            .get('rux-notification')
+            .first()
+            .shadow()
+            .find('rux-icon')
+        icon.click()
+        notification.should('not.have.attr', 'open')
+    })
 })
