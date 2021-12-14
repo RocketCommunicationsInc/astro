0dd6520d71c688ddb3a864b8dfaa78d0290da071
Micah Jones <micah.jones@rocketcom.com>
2021-12-14 11:40:40 -0700

    test(modal): added test to check for close event firing once

--- packages/web-components/src/components/rux-modal/test/rux-modal.spec.tsx
+++ packages/web-components/src/components/rux-modal/test/rux-modal.spec.tsx
@@ -1,5 +1,4 @@
-import { h } from '@stencil/core'
-import { newSpecPage, SpecPage } from '@stencil/core/testing'
+import { newSpecPage } from '@stencil/core/testing'
 import { RuxModal } from '../rux-modal'
 
 describe('rux-modal', () => {
@@ -13,36 +12,5 @@ describe('rux-modal', () => {
                 <mock:shadow-root></mock:shadow-root>
             </rux-modal>
         `)
-    })
-    it('should emit one event when closed', async () => {
-        const clickSpy = jest.fn()
-        let page: SpecPage
-
-        page = await newSpecPage({
-            components: [RuxModal],
-            template: () => (
-                <div>
-                    <div id="click">Something to click</div>
-                    <rux-modal
-                        click-to-close
-                        open
-                        onRuxmodalclosed={(ev: any) => clickSpy(ev)}
-                    >
-                        <div slot="header">Header</div>
-                    </rux-modal>
-                </div>
-            ),
-        })!
-
-        page.doc?.addEventListener('onruxclosed', () => {
-            console.log('heard')
-        })
-        const modal = page.doc?.querySelector('rux-modal')
-        expect(modal!.open).toBe(true)
-        const div = page.doc?.getElementById('click')
-        // modal!.open = false
-        div!.click()
-        // expect(modal!.open).toBe(false)
-        expect(clickSpy).toBeCalledTimes(1)
     })
 })
