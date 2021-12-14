bab8e0ebd7179c3daae257bc33d641c634c64bad
Micah Jones <micah.jones@rocketcom.com>
2021-12-14 11:40:40 -0700

    test(modal): added test to check for close event firing once

--- packages/web-components/src/components/rux-modal/test/rux-modal.e2e.js
+++ packages/web-components/src/components/rux-modal/test/rux-modal.e2e.js
@@ -14,7 +14,7 @@ describe('Modal', () => {
         })
         cy.get('rux-modal').shadow().find('.rux-modal__wrapper').should('exist')
     })
-    it('should not on off click if click-to-close if false', () => {
+    it('should not close on off click if click-to-close if false', () => {
         cy.get('rux-modal')
             .shadow()
             .find('.rux-modal__wrapper')
@@ -33,6 +33,24 @@ describe('Modal', () => {
             .shadow()
             .find('.rux-modal__wrapper')
             .should('not.exist')
+    })
+    it('should fire ruxmodalclosed once if clicked to close', async () => {
+        let count = 0
+
+        cy.get('rux-modal').then(($modal) => {
+            $modal[0].setAttribute('click-to-close', true)
+            $modal[0].setAttribute('open', true)
+        })
+        cy.document().then(($doc) => {
+            $doc.addEventListener('ruxmodalclosed', () => {
+                count++
+            })
+        })
+        cy.get('rux-modal')
+            .shadow()
+            .find('.rux-modal__wrapper')
+            .click('topLeft')
+            .then(() => expect(count).to.equal(1))
     })
     it('should have a footer el if footer slot is used', () => {
         cy.get('rux-modal').then(($modal) => {
