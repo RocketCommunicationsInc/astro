7b57c0fef30cb02f793d54681d2f60caefd7eea7
NicDominguez <nic.dominguez@rocketcom.com>
2021-12-23 09:03:31 -0800

    feat(sveltekit-starter): implments astro components

--- packages/starter-kits/sveltekit-starter/src/lib/header/Header.svelte
+++ packages/starter-kits/sveltekit-starter/src/lib/header/Header.svelte
@@ -8,6 +8,7 @@
 		<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 		<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
 	</ul>
+	<rux-clock slot="right-side" />
 </rux-global-status-bar>
 
 <style>
@@ -18,5 +19,11 @@
 
 	li {
 		margin: 0 20px;
+		font-size: 20px;
+		color: white;
+	}
+
+	a {
+		color: white;
 	}
 </style>
