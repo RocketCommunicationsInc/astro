649fff5d3674134b3b3e871e083119a2dd695075
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-01-06 15:37:54 -0500

    docs: update framework importing (#188)
    * docs: fix vue importing
    
    * fix(starter-kits): import from loader
    
    * fix(starter-kits): svelte import from loader

--- packages/starter-kits/svelte-starter/index.js
+++ packages/starter-kits/svelte-starter/index.js
@@ -2,9 +2,16 @@ import App from "./App.svelte";
 
 // Import Astro's base styles
 import "../node_modules/@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css";
+
 // Define the Astro Components
-import { defineCustomElements } from "@astrouxds/astro-web-components/dist/custom-elements";
-defineCustomElements();
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from "@astrouxds/astro-web-components/loader";
+
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
 
 const app = new App({
   target: document.body,
