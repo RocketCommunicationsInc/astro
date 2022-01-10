dac0cd9a8b330df45cde51a4ce3a01a44f21e772
Micah Jones <micah.jones@rocketcom.com>
2022-01-10 09:47:19 -0700

    chore(modal): swapped button parts to correct spots

--- packages/web-components/src/components/rux-modal/rux-modal.tsx
+++ packages/web-components/src/components/rux-modal/rux-modal.tsx
@@ -166,7 +166,7 @@ export class RuxModal {
                                         data-value="false"
                                         hidden={!denyText}
                                         tabindex="-1"
-                                        exportparts="container:confirm-button"
+                                        exportparts="container:deny-button"
                                     >
                                         {denyText}
                                     </rux-button>
@@ -175,7 +175,7 @@ export class RuxModal {
                                         data-value="true"
                                         hidden={!confirmText}
                                         tabindex="0"
-                                        exportparts="container:deny-button"
+                                        exportparts="container:confirm-button"
                                     >
                                         {confirmText}
                                     </rux-button>
