164cf3f2bbb664b36dbe80d7652667325c927d00
Micah Jones <micah.jones@rocketcom.com>
2022-01-10 10:47:41 -0700

    feat(checkbox): added form-field and label parts

--- packages/web-components/src/components/rux-checkbox/rux-checkbox.tsx
+++ packages/web-components/src/components/rux-checkbox/rux-checkbox.tsx
@@ -146,7 +146,7 @@ export class RuxCheckbox implements FormFieldInterface {
 
         return (
             <Host>
-                <div class="rux-form-field">
+                <div class="rux-form-field" part="form-field">
                     <div
                         class={{
                             'rux-checkbox': true,
@@ -168,7 +168,7 @@ export class RuxCheckbox implements FormFieldInterface {
                             onBlur={this._onBlur}
                             ref={(el) => (this._inputEl = el)}
                         />
-                        <label htmlFor={checkboxId}>
+                        <label htmlFor={checkboxId} part="label">
                             {this.label}
                             <span
                                 class={{
