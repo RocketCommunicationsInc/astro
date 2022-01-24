52764d6152e08c46be460f9bd662ce71e03193df
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-01-24 16:08:27 -0500

    feat(rux-timeline): support for off margins

--- packages/web-components/src/components/rux-timeline/rux-time-region/rux-time-region.tsx
+++ packages/web-components/src/components/rux-timeline/rux-time-region/rux-time-region.tsx
@@ -1,4 +1,7 @@
-import { Element, Prop, Component, Host, h } from '@stencil/core'
+import { Element, Prop, Component, State, Host, h } from '@stencil/core'
+import differenceInMinutes from 'date-fns/differenceInMinutes'
+import { differenceInHours } from 'date-fns/esm'
+import format from 'date-fns/format'
 
 @Component({
     tag: 'rux-time-region',
@@ -7,15 +10,16 @@ import { Element, Prop, Component, Host, h } from '@stencil/core'
 })
 export class RuxTimeRegion {
     // private ruxTrack: HTMLRuxTrackElement | null = null
+    private ruxTimeline: any
     @Element() el!: HTMLRuxTimeRegionElement
     /**
      * The start time
      */
-    @Prop() start: string = '0000'
+    @Prop() start: any
     /**
      * The end time
      */
-    @Prop() end: string = '2359'
+    @Prop() end: any
 
     /**
      * The label
@@ -27,7 +31,25 @@ export class RuxTimeRegion {
      */
     @Prop() track: string = '1'
 
+    @State() startOffset = 0
+    @State() endOffset = 0
     componentWillLoad() {
+        const start = new Date(this.start)
+        const end = new Date(this.end)
+        const length = Math.abs(differenceInMinutes(start, end))
+        console.log('length', length)
+        this.ruxTimeline = this.el?.closest('rux-timeline')
+
+        // if (length < 60) {
+
+        console.log('iii', this.ratio)
+
+        console.log('end offset', end.getMinutes())
+
+        this.startOffset = start.getMinutes() * this.ratio
+        this.endOffset = end.getMinutes() * this.ratio
+        // }
+
         // console.log('region track', this.track)
         // this.ruxTrack = this.el.closest('rux-track')
         // console.log('track', this.ruxTrack);
@@ -39,8 +61,35 @@ export class RuxTimeRegion {
         // this.track = id ? id : '1'
     }
 
-    calculateGridColumnFromTime(time: string) {
-        return +time.substring(0, 2) + 2
+    get ratio() {
+        if (this.ruxTimeline.interval === 'hour') {
+            return this.ruxTimeline.zoom / 60 // for hours.
+        }
+
+        if (this.ruxTimeline.interval === 'day') {
+            return this.ruxTimeline.zoom / 120 //tbd
+        }
+        return 2
+    }
+
+    calculateGridColumnFromTime(time: any) {
+        console.log('test', this.ruxTimeline.start)
+        const timelineStart = new Date(this.ruxTimeline.start)
+        const difference = Math.abs(
+            differenceInHours(timelineStart, new Date(time))
+        )
+        console.log('diff in hour', difference)
+
+        return difference + 2
+        // console.log('dif', difference);
+
+        // const date = new Date(time)
+        // const form = format(date, 'HH')
+        // console.log('form', form);
+
+        // // return new Date(time).getHours() + 2
+        // return +form + 2
+        // return +time.substring(0, 2) + 2
     }
 
     render() {
@@ -54,13 +103,16 @@ export class RuxTimeRegion {
                         gridColumn: `${this.calculateGridColumnFromTime(
                             this.start
                         )} / ${this.calculateGridColumnFromTime(this.end)}`,
+                        marginLeft: `${this.startOffset}px`,
+                        marginRight: `${this.endOffset}px`,
                     }}
                 >
                     <div class="rux-time-region__content">
                         <slot></slot>
                     </div>
                     <div class="rux-time-region__datetime">
-                        {this.start} - {this.end}
+                        {format(new Date(this.start), 'HH:mm')} -{' '}
+                        {format(new Date(this.end), 'HH:mm')}
                     </div>
                 </div>
             </Host>
