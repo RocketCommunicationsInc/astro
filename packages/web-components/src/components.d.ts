42ef2386eaa3c2be8acb6b82e5148fb717d5e1e2
Micah Jones <micah.jones@rocketcom.com>
2022-03-16 10:26:23 -0600

    style(modal): updating padding

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -11978,21 +11978,9 @@ export namespace Components {
     }
     interface RuxModal {
         /**
-          * Text for confirmation button
+          * Allows modal to close by clicking off of it
          */
-        "confirmText": string;
-        /**
-          * Text for close button
-         */
-        "denyText": string;
-        /**
-          * Modal body message
-         */
-        "modalMessage"?: string;
-        /**
-          * Modal header title
-         */
-        "modalTitle"?: string;
+        "clickToClose": boolean;
         /**
           * Shows and hides modal
          */
@@ -32154,25 +32142,17 @@ declare namespace LocalJSX {
     }
     interface RuxModal {
         /**
-          * Text for confirmation button
+          * Allows modal to close by clicking off of it
          */
-        "confirmText"?: string;
-        /**
-          * Text for close button
-         */
-        "denyText"?: string;
-        /**
-          * Modal body message
-         */
-        "modalMessage"?: string;
-        /**
-          * Modal header title
-         */
-        "modalTitle"?: string;
+        "clickToClose"?: boolean;
         /**
           * Event that is fired when modal closes
          */
         "onRuxmodalclosed"?: (event: CustomEvent<boolean>) => void;
+        /**
+          * Event that is fired when modal opens
+         */
+        "onRuxmodalopened"?: (event: CustomEvent<boolean>) => void;
         /**
           * Shows and hides modal
          */
