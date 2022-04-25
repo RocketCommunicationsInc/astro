5eb97493c8948952829501f573cbcbae0a295880
Micah Jones <micah.jones@rocketcom.com>
2022-04-25 14:14:06 -0600

    test: rm test folder

--- packages/web-components/src/components/rux-indeterminate-progress/test/rux-indeterminate-progress.e2e.js
+++
@@ -1,8 +0,0 @@
-describe('Indeterminate Progress', () => {
-    beforeEach(() => {
-        cy.visitComponent('rux-indeterminate-progress')
-    })
-    it('renders', () => {
-        cy.get('rux-indeterminate-progress').should('have.class', 'hydrated')
-    })
-})
