3452f931a6c8a14687446784e110bf7fc2c9279f
Micah Jones <micah.jones@rocketcom.com>
2022-04-25 11:05:30 -0600

    test(ind-prog): added index test file, updated rux-progress test

--- packages/web-components/src/components/rux-progress/test/rux-progress.e2e.js
+++ packages/web-components/src/components/rux-progress/test/rux-progress.e2e.js
@@ -5,7 +5,7 @@ describe('Progress', () => {
     it('renders', () => {
         cy.get('rux-progress').should('have.class', 'hydrated')
     })
-    it('renders rux-spinner when no value is present', () => {
+    it('renders rux-indeterminate-progress when no value is present', () => {
         cy.get('#spinner').shadow().children().should('have.length', '3')
     })
 })
