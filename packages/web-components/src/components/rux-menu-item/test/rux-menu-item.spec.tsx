5ac9b9f6f075d18c91495f5515f563d88075a933
Micah Jones <micah.jones@rocketcom.com>
2022-04-28 10:40:26 -0600

    test: update snapshot on menu-item spec

--- packages/web-components/src/components/rux-menu-item/test/rux-menu-item.spec.tsx
+++ packages/web-components/src/components/rux-menu-item/test/rux-menu-item.spec.tsx
@@ -1,7 +1,7 @@
 import { newSpecPage } from '@stencil/core/testing'
 import { RuxMenuItem } from '../rux-menu-item'
 
-describe('rux-beta-menu-item', () => {
+describe('rux-menu-item', () => {
     it('renders', async () => {
         const page = await newSpecPage({
             components: [RuxMenuItem],
