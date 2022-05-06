2e1cb4e28b2f471e80c8db32adac79da8d263767
Micah Jones <micah.jones@rocketcom.com>
2022-05-06 09:47:56 -0600

    test(ind-prog): removed small test

--- packages/web-components/src/components/rux-indeterminate-progress/test/rux-indeterminate-progress.e2e.js
+++ packages/web-components/src/components/rux-indeterminate-progress/test/rux-indeterminate-progress.e2e.js
@@ -5,13 +5,4 @@ describe('Indeterminate Progress', () => {
     it('renders', () => {
         cy.get('rux-indeterminate-progress').should('have.class', 'hydrated')
     })
-    it('applies the small class if below 30px size', () => {
-        cy.get('#small').shadow().find('div').should('have.class', 'small')
-    })
-    it('should not apply the small class if --size is above 30px', () => {
-        cy.get('#almost-small')
-            .shadow()
-            .find('div')
-            .should('not.have.class', 'small')
-    })
 })
