0c6b9e8c1cd462ff4729c9bdbf8d0ce16b5f7965
Micah Jones <micah.jones@rocketcom.com>
2022-05-11 10:48:12 -0600

    test: uncomment

--- packages/web-components/src/components/rux-monitoring-progress-icon/test/rux-monitoring-progress-icon.spec.tsx
+++ packages/web-components/src/components/rux-monitoring-progress-icon/test/rux-monitoring-progress-icon.spec.tsx
@@ -17,58 +17,58 @@ describe('rux-monitoring-progress-icon', () => {
         expect(page.root).toMatchSnapshot()
     })
 
-    // it('applies custom range arrays properly', () => {
-    //     const progressIcon = new RuxMonitoringProgressIcon()
-    //     progressIcon.min = 100
-    //     progressIcon.max = 1100
-    //     progressIcon.progress = 675
-    //     progressIcon.range = [
-    //         {
-    //             threshold: 300,
-    //             status: 'standby',
-    //         },
-    //         {
-    //             threshold: 775,
-    //             status: 'normal',
-    //         },
-    //         {
-    //             threshold: 1100,
-    //             status: 'serious',
-    //         },
-    //     ]
-    //     progressIcon._updateProgress()
-    //     expect(progressIcon.status).toBe('normal')
-    //     expect(
-    //         Math.ceil(
-    //             ((progressIcon.progress - progressIcon.min) /
-    //                 (progressIcon.max - progressIcon.min)) *
-    //                 100
-    //         )
-    //     ).toBe(58)
+    it('applies custom range arrays properly', () => {
+        const progressIcon = new RuxMonitoringProgressIcon()
+        progressIcon.min = 100
+        progressIcon.max = 1100
+        progressIcon.progress = 675
+        progressIcon.range = [
+            {
+                threshold: 300,
+                status: 'standby',
+            },
+            {
+                threshold: 775,
+                status: 'normal',
+            },
+            {
+                threshold: 1100,
+                status: 'serious',
+            },
+        ]
+        progressIcon._updateProgress()
+        expect(progressIcon.status).toBe('normal')
+        expect(
+            Math.ceil(
+                ((progressIcon.progress - progressIcon.min) /
+                    (progressIcon.max - progressIcon.min)) *
+                    100
+            )
+        ).toBe(58)
 
-    //     progressIcon.progress = 1100
-    //     progressIcon._updateProgress()
-    //     expect(progressIcon.status).toBe('serious')
-    // })
+        progressIcon.progress = 1100
+        progressIcon._updateProgress()
+        expect(progressIcon.status).toBe('serious')
+    })
 
-    // it('works with empty range array', () => {
-    //     const progressIcon = new RuxMonitoringProgressIcon()
+    it('works with empty range array', () => {
+        const progressIcon = new RuxMonitoringProgressIcon()
 
-    //     progressIcon.progress = 50
-    //     progressIcon.range = []
-    //     progressIcon.componentWillLoad()
-    //     expect(progressIcon.status).toBe('caution')
-    // })
-    // it('changes progress value to equal max if passed in progress is greater', () => {
-    //     const progressIcon = new RuxMonitoringProgressIcon()
-    //     progressIcon.progress = 101
-    //     progressIcon.componentWillLoad()
-    //     expect(progressIcon.progress).toEqual(progressIcon.max)
-    // })
-    // it('changes progress value to equal min if passed in progress is less', () => {
-    //     const progressIcon = new RuxMonitoringProgressIcon()
-    //     progressIcon.progress = -10
-    //     progressIcon.componentWillLoad()
-    //     expect(progressIcon.progress).toEqual(progressIcon.min)
-    // })
+        progressIcon.progress = 50
+        progressIcon.range = []
+        progressIcon.componentWillLoad()
+        expect(progressIcon.status).toBe('caution')
+    })
+    it('changes progress value to equal max if passed in progress is greater', () => {
+        const progressIcon = new RuxMonitoringProgressIcon()
+        progressIcon.progress = 101
+        progressIcon.componentWillLoad()
+        expect(progressIcon.progress).toEqual(progressIcon.max)
+    })
+    it('changes progress value to equal min if passed in progress is less', () => {
+        const progressIcon = new RuxMonitoringProgressIcon()
+        progressIcon.progress = -10
+        progressIcon.componentWillLoad()
+        expect(progressIcon.progress).toEqual(progressIcon.min)
+    })
 })
