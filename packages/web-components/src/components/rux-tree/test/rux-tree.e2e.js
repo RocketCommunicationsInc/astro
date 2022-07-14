8cf0f224c41f287da4d7f88a294d36688dd0c4f4
Micah Jones <48889844+micahjones13@users.noreply.github.com>
2022-07-14 15:47:54 -0600

    Astro 4081 tree event (#574)
    * feat(tree): added expanded and collapsed events to tree-node
    
    * test(tree): added logs for events in test index
    
    * test(ga): install stencil if cache not found for e2e
    
    * test(ga): cache refactor
    
    * build: stencil build
    
    * docs(tree): added new events
    
    * docs: changeset

--- packages/web-components/src/components/rux-tree/test/rux-tree.e2e.js
+++ packages/web-components/src/components/rux-tree/test/rux-tree.e2e.js
@@ -16,4 +16,39 @@ describe('Tree', () => {
         cy.realPress('ArrowLeft')
         cy.focused().should('not.have.attr', 'expanded')
     })
+    it('emits ruxtreenodeselected event', () => {
+        cy.document().invoke(
+            'addEventListener',
+            'ruxtreenodeselected',
+            cy.stub().as('ruxtreenodeselected')
+        )
+        cy.get('rux-tree-node').first().shadow().find('.parent').click()
+        cy.get('@ruxtreenodeselected').should('be.calledOnce')
+    })
+    it('emits ruxtreenodeexpanded', () => {
+        cy.document().invoke(
+            'addEventListener',
+            'ruxtreenodeexpanded',
+            cy.stub().as('ruxtreenodeexpanded')
+        )
+        cy.get('#test-expanded').shadow().find('.parent').find('.arrow').click()
+        cy.get('@ruxtreenodeexpanded')
+            .should('be.calledOnce')
+            .its('firstCall.args.0.detail')
+            .should('include', 'node')
+    })
+    it('emits ruxtreenodecollapsed', () => {
+        cy.document().invoke(
+            'addEventListener',
+            'ruxtreenodecollapsed',
+            cy.stub().as('ruxtreenodecollapsed')
+        )
+        // Need to open it first to test
+        cy.get('#test-expanded').shadow().find('.parent').find('.arrow').click()
+        cy.get('#test-expanded').shadow().find('.parent').find('.arrow').click()
+        cy.get('@ruxtreenodecollapsed')
+            .should('be.calledOnce')
+            .its('firstCall.args.0.detail')
+            .should('include', 'node')
+    })
 })
