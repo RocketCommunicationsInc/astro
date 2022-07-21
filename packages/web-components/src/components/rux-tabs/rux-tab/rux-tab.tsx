d6a6a1ae865e41a5647de5b61e50a496e650f2a9
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-07-21 10:35:16 -0400

    fix(rux-tabs): can no longer change shadow dom typography styles

--- packages/web-components/src/components/rux-tabs/rux-tab/rux-tab.tsx
+++ packages/web-components/src/components/rux-tabs/rux-tab/rux-tab.tsx
@@ -15,6 +15,11 @@ export class RuxTab {
      */
     @Prop({ reflect: true }) disabled: boolean = false
 
+    /**
+     * If passed or set to true, displays the tabs in a smaller style, suitable for limited-space uses.
+     */
+    @Prop() small: boolean = false
+
     @Element() el!: HTMLRuxTabElement
 
     connectedCallback() {
@@ -40,6 +45,8 @@ export class RuxTab {
                     class={{
                         'rux-tab': true,
                         'rux-tab--selected': this.selected,
+                        'rux-tab--small': this.small,
+                        'rux-tab--large': !this.small,
                         'rux-tab--disabled': this.disabled,
                     }}
                 >
