3f4f9b8b2f0eaffad9dfcf0e5e8122e434a7800d
Micah Jones <micah.jones@rocketcom.com>
2022-08-30 11:48:23 -0600

    test(GSB): added playwright test

---
+++ packages/web-components/tests/global-status-bar.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test'
+import { startTestEnv } from './utils/_startTestEnv'
+
+test.describe('Global status bar', () => {
+    startTestEnv()
+
+    test('it renders', async ({ page }) => {
+        await page.setContent(`
+        <rux-global-status-bar></rux-global-status-bar>
+        `)
+        const el = page.locator('rux-global-status-bar')
+        expect(el).toHaveClass('hydrated')
+    })
+    /**
+     * Need to test:
+     * - Attributes
+     * - It can render other comps, ie clock?
+     * -
+     */
+})
