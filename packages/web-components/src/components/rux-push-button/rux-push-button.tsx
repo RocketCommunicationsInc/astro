d054e74bc67c4c8aad4b0377df3e0b6985d273d7
Kiley Mitti <kiley.mitti@rocketcom.com>
2022-08-31 13:01:27 -0400

    add spacing tokens and test

--- packages/web-components/src/components/rux-push-button/rux-push-button.tsx
+++ packages/web-components/src/components/rux-push-button/rux-push-button.tsx
@@ -140,7 +140,7 @@ export class RuxPushButton {
                 >
                     {icon ? (
                         <rux-icon
-                            size="extra-small"
+                            size="auto"
                             exportparts="icon"
                             icon={icon}
                         ></rux-icon>
