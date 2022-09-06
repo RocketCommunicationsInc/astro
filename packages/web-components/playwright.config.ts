c4c24f9862d0a6233c5bf65fbbd2d1d11674ce6c
Micah Jones <micah.jones@rocketcom.com>
2022-09-06 14:37:17 -0600

    config: playwright config

--- packages/web-components/playwright.config.ts
+++ packages/web-components/playwright.config.ts
@@ -36,7 +36,7 @@ const config: PlaywrightTestConfig = {
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
         actionTimeout: 0,
         /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://localhost:3000',
+        baseURL: 'http://localhost:3333',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -100,7 +100,7 @@ const config: PlaywrightTestConfig = {
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run start.stencil',
-        port: 3333,
+        url: 'http://localhost:3333/',
     },
 }
 
