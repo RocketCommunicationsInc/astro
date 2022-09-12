e4660320f8cf194ee59db17ef1b8c8c998fe87ba
Brian <brian@briankiernan.com>
2022-09-12 19:33:41 -0400

    chore(FormFieldMessage): remove commented out markup

--- packages/web-components/src/common/functional-components/FormFieldMessage/FormFieldMessage.tsx
+++ packages/web-components/src/common/functional-components/FormFieldMessage/FormFieldMessage.tsx
@@ -35,29 +35,6 @@ const FormFieldMessage: FunctionalComponent<FormFieldMessageProps> = (
     }
 
     return null
-
-    // function shouldShowHelpText() {
-    //     return helpText && !errorText
-    // }
-
-    // function shouldShowErrorText() {
-    //     return errorText ? true : false
-    // }
-
-    // return (
-    //     (shouldShowHelpText() ? (
-    //         <div class="rux-help-text" part="help-text">
-    //             {children}
-    //             {helpText}
-    //         </div>
-    //     ) : null) ||
-    //     (shouldShowErrorText() ? (
-    //         <div class="rux-error-text" part="error-text">
-    //             {children}
-    //             {errorText}
-    //         </div>
-    //     ) : null)
-    // )
 }
 
 export default FormFieldMessage
