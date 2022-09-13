8b234fe9ef7bb1efc2198cb41ddbcd8882354332
Micah Jones <micah.jones@rocketcom.com>
2022-09-13 11:22:31 -0600

    feat(status): changed status symbols from png to svg. Updated svg rollup

--- packages/web-components/src/components/rux-status/rux-status.tsx
+++ packages/web-components/src/components/rux-status/rux-status.tsx
@@ -1,16 +1,37 @@
-import { Prop, Host, Component, h, Watch } from '@stencil/core'
+import { Prop, Host, Component, h, Watch, State, Element } from '@stencil/core'
 import { Status, StatusTypes } from '../../common/commonTypes.module'
+import DarkStatus from './statuses/dark-theme'
+import LightStatus from './statuses/light-theme'
 
 @Component({
     tag: 'rux-status',
     styleUrl: 'rux-status.scss',
     shadow: true,
 })
+
+/**
+ * @part status - the container of the rux-status symbol
+ */
 export class RuxStatus {
     /**
      * Sets the status symbol, valid options are critical, serious, caution, normal, standby and off
      */
     @Prop({ reflect: true }) status?: Status
+
+    @Element() el!: HTMLRuxStatusElement
+
+    @State() theme: string = 'dark'
+
+    /**
+     * Mutation observer to watch for class changes on body. Allows for theme switching.
+     */
+    private classObserver = new MutationObserver((mutations) => {
+        mutations.forEach((mu) => {
+            //Only want to listen for specifc types
+            if (mu.type !== 'attributes' && mu.attributeName !== 'class') return
+            this._determineTheme()
+        })
+    })
 
     @Watch('status')
     validateStatus(newValue: string) {
@@ -24,11 +45,47 @@ export class RuxStatus {
         } as StatusTypes
 
         if (!statusTypes[newValue]) {
-            throw new Error('valid status required')
+            throw new Error(
+                `rux-status: status of ${this.status} is not valid. Please use a valid status.`
+            )
         }
     }
 
+    connectedCallback() {
+        if (this.status) this.validateStatus(this.status)
+        this._determineTheme()
+
+        const config = { attributes: true }
+        this.classObserver.observe(document.querySelector('body')!, config)
+    }
+
+    disconnectedCallback() {
+        this.classObserver.disconnect()
+    }
+    private _determineTheme() {
+        if (
+            this.el.classList.contains('light-theme') ||
+            document.querySelector('body')?.classList.contains('light-theme')
+        ) {
+            this.theme = 'light'
+        } else this.theme = 'dark'
+    }
+
     render() {
-        return <Host status={this.status} />
+        return (
+            <Host status={this.status}>
+                <div
+                    part="status"
+                    class="status-wrapper"
+                    innerHTML={
+                        this.status && this.theme === 'dark'
+                            ? DarkStatus[this.status]
+                            : this.status && this.theme === 'light'
+                            ? LightStatus[this.status]
+                            : undefined
+                    }
+                ></div>
+            </Host>
+        )
     }
 }
---
+++ packages/web-components/src/components/rux-status/statuses/dark-theme/index.ts
@@ -0,0 +1,17 @@
+import caution from './Caution-Dark.svg'
+import critical from './Critical-Dark.svg'
+import normal from './Normal-Dark.svg'
+import off from './Off-Dark.svg'
+import serious from './Serious-Dark.svg'
+import standby from './Standby-Dark.svg'
+
+const DarkStatus = {
+    caution,
+    critical,
+    normal,
+    off,
+    serious,
+    standby,
+}
+
+export default DarkStatus
