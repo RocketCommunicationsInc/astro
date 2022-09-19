1d8131995185fcb95908927e80d90e827b715569
Micah Jones <micah.jones@rocketcom.com>
2022-09-19 15:13:58 -0600

    refactor(status): removed observer and extra steps, added some test code

--- packages/web-components/src/components/rux-status/rux-status.tsx
+++ packages/web-components/src/components/rux-status/rux-status.tsx
@@ -1,4 +1,4 @@
-import { Prop, Host, Component, h, Watch, State, Element } from '@stencil/core'
+import { Prop, Host, Component, h, Watch, Element } from '@stencil/core'
 import { Status, StatusTypes } from '../../common/commonTypes.module'
 // import DarkStatus from './statuses/dark-theme'
 import StatusSVG from './statuses/light-theme'
@@ -20,19 +20,6 @@ export class RuxStatus {
 
     @Element() el!: HTMLRuxStatusElement
 
-    @State() theme: string = 'dark'
-
-    /**
-     * Mutation observer to watch for class changes on body. Allows for theme switching.
-     */
-    private _classObserver = new MutationObserver((mutations) => {
-        mutations.forEach((mu) => {
-            //Only want to listen for specifc types
-            if (mu.type !== 'attributes' && mu.attributeName !== 'class') return
-            this._determineTheme()
-        })
-    })
-
     @Watch('status')
     validateStatus(newValue: string) {
         const statusTypes = {
@@ -53,28 +40,9 @@ export class RuxStatus {
 
     connectedCallback() {
         if (this.status) this.validateStatus(this.status)
-        this._determineTheme()
-
-        const config = { attributes: true }
-        //Observe classlist changes for body and rux-status (this.el)
-        this._classObserver.observe(document.querySelector('body')!, config)
-        this._classObserver.observe(this.el, config)
-    }
-
-    disconnectedCallback() {
-        this._classObserver.disconnect()
-    }
-    private _determineTheme() {
-        if (
-            this.el.classList.contains('light-theme') ||
-            document.querySelector('body')?.classList.contains('light-theme')
-        ) {
-            this.theme = 'light'
-        } else this.theme = 'dark'
     }
 
     render() {
-        console.log(StatusSVG[this.status!])
         return (
             <Host status={this.status}>
                 <div
