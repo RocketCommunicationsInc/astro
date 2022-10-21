35d56cb5d4d51ae14a3d3fb5c9f3dcaa1dfa90df
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-10-21 19:48:28 -0400

    test: btn group

--- packages/web-components/tests/classification-markings.spec.ts
+++ packages/web-components/tests/classification-markings.spec.ts
@@ -2,9 +2,7 @@ import { test, expect } from './utils/_astro-fixtures'
 
 test.describe('Classification Marking', () => {
     test.describe('Tags', () => {
-        test('displays correct label for Unclassified', async ({
-            astroPage,
-        }) => {
+        test('displays correct label for Unclassified', async ({ astroPage }) => {
             const template = `
                 <rux-classification-marking 
                     tag 
@@ -31,9 +29,7 @@ test.describe('Classification Marking', () => {
             await expect(el).toHaveText('cui')
         })
 
-        test('displays the correct label for Confidential', async ({
-            astroPage,
-        }) => {
+        test('displays the correct label for Confidential', async ({ astroPage }) => {
             const template = `<rux-classification-marking tag classification="confidential"></rux-classification-marking>`
 
             const el = await astroPage.load(template)
@@ -47,174 +43,101 @@ test.describe('Classification Marking', () => {
             await expect(el).toHaveText('s')
         })
 
-        // test('displays the correct label for Top Secret', async ({ astroPage }) => {
-        //     await setBodyContent(
-        //         page,
-        //         `
-        //     <rux-classification-marking tag classification="top-secret"></rux-classification-marking>
-        // `
-        //     )
+        test('displays the correct label for Top Secret', async ({ astroPage }) => {
+            const template = `<rux-classification-marking tag classification="top-secret"></rux-classification-marking>`
 
-        //     const el = page.locator('rux-classification-marking')
-        //     await expect(el).toHaveText('ts')
-        // })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('ts')
+        })
 
-        // test('displays the correct label for Top Secret//SCI', async ({
-        //     page,
-        // }) => {
-        //     await setBodyContent(
-        //         page,
-        //         `
-        //     <rux-classification-marking tag classification="top-secret-sci"></rux-classification-marking>
-        // `
-        //     )
+        test('displays the correct label for Top Secret//SCI', async ({ astroPage }) => {
+            const template = `<rux-classification-marking tag classification="top-secret-sci"></rux-classification-marking>`
 
-        //     const el = page.locator('rux-classification-marking')
-        //     await expect(el).toHaveText('TS//SCI')
-        // })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('TS//SCI')
+        })
     })
 
-    // test.describe('Banners', () => {
-    //     test('it renders', async ({ astroPage }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `<rux-classification-marking></rux-classification-marking>`
-    //         )
+    test.describe('Banners', () => {
+        test('it renders', async ({ astroPage }) => {
+            const template = `<rux-classification-marking></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toBeVisible()
-    //         await expect(el).toHaveClass('hydrated')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toBeVisible()
+            await expect(el).toHaveClass('hydrated')
+        })
 
-    //     test('it sets a custom label', async ({ astroPage }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //         <rux-classification-marking classification="secret" label="Label"></rux-classification-marking>
-    //     `
-    //         )
+        test('it sets a custom label', async ({ astroPage }) => {
+            const template = `<rux-classification-marking classification="secret" label="Label"></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('secretLabel')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('secretLabel')
+        })
 
-    //     test('displays the correct label for Unclassified', async ({
-    //         page,
-    //     }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //         <rux-classification-marking classification="unclassified"></rux-classification-marking>
-    //     `
-    //         )
+        test('displays the correct label for Unclassified', async ({ astroPage }) => {
+            const template = `<rux-classification-marking classification="unclassified"></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('unclassified')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('unclassified')
+        })
 
-    //     test('displays the correct label for Controlled', async ({ astroPage }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //             <rux-classification-marking classification="controlled"></rux-classification-marking>
-    //         `
-    //         )
+        test('displays the correct label for Controlled', async ({ astroPage }) => {
+            const template = `<rux-classification-marking classification="controlled"></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('controlled')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('controlled')
+        })
 
-    //     test('displays the correct label for CUI', async ({ astroPage }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //         <rux-classification-marking classification="cui"></rux-classification-marking>
-    //     `
-    //         )
+        test('displays the correct label for CUI', async ({ astroPage }) => {
+            const template = `<rux-classification-marking classification="cui"></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('cui')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('cui')
+        })
 
-    //     test('displays the correct label for Confidential', async ({
-    //         page,
-    //     }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //         <rux-classification-marking classification="confidential"></rux-classification-marking>
-    //     `
-    //         )
+        test('displays the correct label for Confidential', async ({ astroPage }) => {
+            const template = `<rux-classification-marking classification="confidential"></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('confidential')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('confidential')
+        })
 
-    //     test('displays the correct label for Secret', async ({ astroPage }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //         <rux-classification-marking classification="secret"></rux-classification-marking>
-    //     `
-    //         )
+        test('displays the correct label for Secret', async ({ astroPage }) => {
+            const template = `<rux-classification-marking classification="secret"></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('secret')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('secret')
+        })
 
-    //     test('displays the correct label for Top Secret', async ({ astroPage }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //         <rux-classification-marking classification="top-secret"></rux-classification-marking>
-    //     `
-    //         )
+        test('displays the correct label for Top Secret', async ({ astroPage }) => {
+            const template = `<rux-classification-marking classification="top-secret"></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('top secret')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('top secret')
+        })
 
-    //     test('displays the correct label for Top SecretSCI', async ({
-    //         page,
-    //     }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //         <rux-classification-marking classification="top-secret-sci"></rux-classification-marking>
-    //     `
-    //         )
+        test('displays the correct label for Top SecretSCI', async ({ astroPage }) => {
+            const template = `<rux-classification-marking classification="top-secret-sci"></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('top secret//sci')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('top secret//sci')
+        })
 
-    //     test('it defaults to unclassified if no classification is provided', async ({
-    //         page,
-    //     }) => {
-    //         await setBodyContent(
-    //             page,
-    //             `
-    //                 <rux-classification-marking></rux-classification-marking>
-    //             `
-    //         )
+        test('it defaults to unclassified if no classification is provided', async ({ astroPage }) => {
+            const template = `<rux-classification-marking></rux-classification-marking>`
 
-    //         const el = page.locator('rux-classification-marking')
-    //         await expect(el).toHaveText('unclassified')
-    //     })
+            const el = await astroPage.load(template)
+            await expect(el).toHaveText('unclassified')
+        })
 
-    //     // @TODO - Implement
-    //     // test('it defaults to unclassified if an invalid classification is provided', async ({ astroPage }) => {
-    //     //     await setBodyContent(
-    //     //         page,
-    //     //         `
-    //     //     <rux-classification-marking classification="farts"></rux-classification-marking>
-    //     // `
-    //     //     )
+        // @TODO - Implement ASTRO-4783
+        // test('it defaults to unclassified if an invalid classification is provided', async ({ astroPage }) => {
+        //     const template = `<rux-classification-marking classification="farts"></rux-classification-marking>`
 
-    //     //     const el = page.locator('rux-classification-marking')
-    //     //     await expect(el).toHaveText('unclassified')
-    //     // })
-    // })
+        //     const el = await astroPage.load(template)
+        //     await expect(el).toHaveText('unclassified')
+        // })
+    })
 
     // @TODO - Implement in VRT
     // test('it renders footer banner')
