cba8e5edd4023c747db73a6d2ba3044dbda5d8dd
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-11-17 15:21:35 -0500

    idk anymore

--- packages/web-components/src/components/rux-notification/test/basic/notification.vrt.spec.ts
+++ packages/web-components/src/components/rux-notification/test/basic/notification.vrt.spec.ts
@@ -3,8 +3,11 @@ import { test, expect } from '../../../../../tests/utils/_astro-fixtures'
 test.describe('Notification', () => {
     test('has no visual regression @vrt', async ({ page }) => {
         await page.goto(
-            `/src/components/rux-notification/test/basic/index.html`
+            `/src/components/rux-notification/test/basic/index.html`,
+            { waitUntil: 'networkidle' }
         )
+        await page.waitForFunction(() => document.fonts.check('1em Roboto'))
+
         await expect(page).toHaveScreenshot()
     })
 })
