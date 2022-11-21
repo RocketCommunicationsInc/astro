afb799d99d171630bf2d56f769b5fbcfbcf78b7c
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-11-21 15:54:09 -0500

    remove wait for robot

--- packages/web-components/src/components/rux-notification/test/basic/notification.vrt.spec.ts
+++ packages/web-components/src/components/rux-notification/test/basic/notification.vrt.spec.ts
@@ -3,10 +3,8 @@ import { test, expect } from '../../../../../tests/utils/_astro-fixtures'
 test.describe('Notification', () => {
     test('has no visual regression @vrt', async ({ page }) => {
         await page.goto(
-            `/src/components/rux-notification/test/basic/index.html`,
-            { waitUntil: 'networkidle' }
+            `/src/components/rux-notification/test/basic/index.html`
         )
-        await page.waitForFunction(() => document.fonts.check('1em Roboto'))
 
         await expect(page).toHaveScreenshot()
     })
