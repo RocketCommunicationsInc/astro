bfec14a966d37543179dc1de37918dc261401ab3
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-11-22 16:06:33 -0500

    test: clock e2e and vrt

--- packages/web-components/src/components/rux-clock/test/clock.spec.ts
+++ packages/web-components/src/components/rux-clock/test/clock.spec.ts
@@ -2,6 +2,28 @@ import { militaryTimezones } from '../military-timezones'
 import { test, expect } from '../../../../tests/utils/_astro-fixtures'
 
 test.describe('Clock', () => {
+    test.beforeEach(async ({ page }) => {
+        const fakeNow = new Date('December 31 2020 20:02:03').valueOf()
+        // const fakeNow = Date.UTC(2021, 0o0, 0o1, 0o1, 0o2, 0o3)
+
+        // Update the Date accordingly in your test pages
+        await page.addInitScript(`{
+          // Extend Date constructor to default to fakeNow
+          Date = class extends Date {
+            constructor(...args) {
+              if (args.length === 0) {
+                super(${fakeNow});
+              } else {
+                super(...args);
+              }
+            }
+          }
+          // Override Date.now() to start from fakeNow
+          const __DateNowOffset = ${fakeNow} - Date.now();
+          const __DateNow = Date.now;
+          Date.now = () => __DateNow() + __DateNowOffset;
+        }`)
+    })
     test('it renders', async ({ page }) => {
         const template = `<rux-clock></rux-clock>`
         await page.setContent(template)
@@ -15,9 +37,13 @@ test.describe('Clock', () => {
             <rux-clock timezone="America/Los_Angeles"></rux-clock>
         `
         await page.setContent(template)
+
         const el = await page.locator('rux-clock')
-        await expect(el).toHaveAttribute('timezone', 'America/Los_Angeles')
+        const segment = await el.locator('.rux-clock__segment__value').nth(1)
+
+        await expect(segment).toHaveText('17:02:03 PST')
     })
+
     test('converts all military timezones', async ({ page }) => {
         for (const timezone in militaryTimezones) {
             const template = `
@@ -25,7 +51,10 @@ test.describe('Clock', () => {
             `
             await page.setContent(template)
             const el = await page.locator('rux-clock')
-            await expect(el).toHaveAttribute('timezone', timezone)
+            const segment = await el
+                .locator('.rux-clock__segment__value')
+                .nth(1)
+            await expect(segment).toHaveText('02:02:03 GMT+1')
         }
     })
 
