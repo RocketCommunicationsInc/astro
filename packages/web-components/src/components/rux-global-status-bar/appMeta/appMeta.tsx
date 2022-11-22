b2207457c819b42b4173634e5ebc57bc7c1dcfc2
Kiley <110477068+kiley-mitti@users.noreply.github.com>
2022-11-22 14:42:32 -0500

    feat(rux-gsb)added parts for app meta and the default slot (#937)
    * added parts for app meta and the default slot
    
    * revert username wording
    
    * add changeset
    
    * remove center test page

--- packages/web-components/src/components/rux-global-status-bar/appMeta/appMeta.tsx
+++ packages/web-components/src/components/rux-global-status-bar/appMeta/appMeta.tsx
@@ -10,7 +10,7 @@ export const AppMeta: FunctionalComponent<AppMetaProps> = (
     { domain, name, version },
     children
 ) => (
-    <div class="app-meta">
+    <div class="app-meta" part="app-meta">
         <div class="app-info-wrapper">
             {domain && <h1 class="app-domain">{domain}</h1>}
             {name && <h1 class="app-name">{name}</h1>}
