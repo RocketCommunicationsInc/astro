bf5b2561a88051627289e0d80b8ab037a17ae37e
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-12-13 16:22:43 -0500

    fix: revert attempt to fix flake

--- packages/web-components/src/components/rux-monitoring-icon/test/basic/monitoring-icon.vrt.spec.ts
+++ packages/web-components/src/components/rux-monitoring-icon/test/basic/monitoring-icon.vrt.spec.ts
@@ -3,30 +3,18 @@ import { test, expect } from '../../../../../tests/utils/_astro-fixtures'
 test.describe('Monitoring-icon', () => {
     test('has no visual regression @vrt @dark', async ({ page }) => {
         await page.goto(
-            `/src/components/rux-monitoring-icon/test/basic/index.html`,
-            { waitUntil: 'networkidle' }
+            `/src/components/rux-monitoring-icon/test/basic/index.html`
         )
-        await page.waitForFunction(() => document.fonts.check('1em Roboto'))
-        const delay = (ms: number) =>
-            new Promise((resolve) => setTimeout(resolve, ms))
-        await delay(2000)
-        await page.waitForChanges()
         await expect(page).toHaveScreenshot()
     })
 
     test('has no visual regression @vrt @light', async ({ page }) => {
         await page.goto(
-            `/src/components/rux-monitoring-icon/test/basic/index.html`,
-            { waitUntil: 'networkidle' }
+            `/src/components/rux-monitoring-icon/test/basic/index.html`
         )
-        await page.waitForFunction(() => document.fonts.check('1em Roboto'))
         await page.evaluate(() => {
             document.body.classList.add('light-theme')
         })
-        const delay = (ms: number) =>
-            new Promise((resolve) => setTimeout(resolve, ms))
-        await delay(2000)
-        await page.waitForChanges()
         await expect(page).toHaveScreenshot()
     })
 })
