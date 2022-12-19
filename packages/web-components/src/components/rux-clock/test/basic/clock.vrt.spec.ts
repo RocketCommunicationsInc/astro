245a6b00fdf978db78c1a4b75e4a7a1603425f4e
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-12-19 09:42:15 -0500

    test: add vrt light theme (#977)
    * add vrt light theme
    
    * fix: revert attempt to fix flake

--- packages/web-components/src/components/rux-clock/test/basic/clock.vrt.spec.ts
+++ packages/web-components/src/components/rux-clock/test/basic/clock.vrt.spec.ts
@@ -22,8 +22,16 @@ test.describe('Clock', () => {
         }`)
     })
 
-    test('has no visual regression @vrt', async ({ page }) => {
+    test('has no visual regression @vrt @dark', async ({ page }) => {
         await page.goto(`/src/components/rux-clock/test/basic/index.html`)
+        await expect(page).toHaveScreenshot()
+    })
+
+    test('has no visual regression @vrt @light', async ({ page }) => {
+        await page.goto(`/src/components/rux-clock/test/basic/index.html`)
+        await page.evaluate(() => {
+            document.body.classList.add('light-theme')
+        })
         await expect(page).toHaveScreenshot()
     })
 })
