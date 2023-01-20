256c28364e6b0518b07c1b5766b487fed9dafee0
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-01-20 13:29:27 -0500

    Adding hover VRTs for button

--- packages/web-components/src/components/rux-button/test/basic/button.vrt.spec.ts
+++ packages/web-components/src/components/rux-button/test/basic/button.vrt.spec.ts
@@ -6,4 +6,31 @@ test.describe('Button', () => {
     test('has no visual regression @vrt', async ({ astroVRTPage }) => {
         await expect(astroVRTPage).toHaveScreenshot()
     })
+    test('primary button hover has no visual regression @vrt', async ({
+        astroVRTPage,
+    }) => {
+        const el = astroVRTPage.locator(
+            'rux-button[data-test-id="primary"] button'
+        )
+        await el.hover()
+        await expect(astroVRTPage).toHaveScreenshot()
+    })
+    test('secondary button hover has no visual regression @vrt', async ({
+        astroVRTPage,
+    }) => {
+        const el = astroVRTPage.locator(
+            'rux-button[data-test-id="secondary"] button'
+        )
+        await el.hover()
+        await expect(astroVRTPage).toHaveScreenshot()
+    })
+    test('borderless button hover has no visual regression @vrt', async ({
+        astroVRTPage,
+    }) => {
+        const el = astroVRTPage.locator(
+            'rux-button[data-test-id="borderless"] button'
+        )
+        await el.hover()
+        await expect(astroVRTPage).toHaveScreenshot()
+    })
 })
