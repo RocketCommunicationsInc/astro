f0d7fa1f02004cf0583567c987806019cff5eaa2
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-01-20 13:42:54 -0500

    modifying button VRTs to not reach into the shadow for selector

--- packages/web-components/src/components/rux-button/test/basic/button.vrt.spec.ts
+++ packages/web-components/src/components/rux-button/test/basic/button.vrt.spec.ts
@@ -9,27 +9,21 @@ test.describe('Button', () => {
     test('primary button hover has no visual regression @vrt', async ({
         astroVRTPage,
     }) => {
-        const el = astroVRTPage.locator(
-            'rux-button[data-test-id="primary"] button'
-        )
+        const el = astroVRTPage.locator('rux-button[data-test-id="primary"]')
         await el.hover()
         await expect(astroVRTPage).toHaveScreenshot()
     })
     test('secondary button hover has no visual regression @vrt', async ({
         astroVRTPage,
     }) => {
-        const el = astroVRTPage.locator(
-            'rux-button[data-test-id="secondary"] button'
-        )
+        const el = astroVRTPage.locator('rux-button[data-test-id="secondary"]')
         await el.hover()
         await expect(astroVRTPage).toHaveScreenshot()
     })
     test('borderless button hover has no visual regression @vrt', async ({
         astroVRTPage,
     }) => {
-        const el = astroVRTPage.locator(
-            'rux-button[data-test-id="borderless"] button'
-        )
+        const el = astroVRTPage.locator('rux-button[data-test-id="borderless"]')
         await el.hover()
         await expect(astroVRTPage).toHaveScreenshot()
     })
