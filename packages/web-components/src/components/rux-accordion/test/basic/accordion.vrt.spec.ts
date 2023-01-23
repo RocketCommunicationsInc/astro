3071d60ac86fbc68959aefe12d214c9ea921e911
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-01-23 13:54:56 -0500

    focus state vrt accordion test

--- packages/web-components/src/components/rux-accordion/test/basic/accordion.vrt.spec.ts
+++ packages/web-components/src/components/rux-accordion/test/basic/accordion.vrt.spec.ts
@@ -13,4 +13,31 @@ test.describe('Accordion', () => {
         })
         await expect(page).toHaveScreenshot()
     })
+
+    test('focus state has no visual regression @vrt @dark', async ({
+        page,
+    }) => {
+        await page.goto(`/src/components/rux-accordion/test/basic/index.html`)
+        await page.evaluate(() => {
+            const focused = document
+                .getElementById('focused')
+                ?.shadowRoot?.querySelector('summary')
+            focused?.focus()
+        })
+        await expect(page).toHaveScreenshot()
+    })
+
+    test('focus state has no visual regression @vrt @light', async ({
+        page,
+    }) => {
+        await page.goto(`/src/components/rux-accordion/test/basic/index.html`)
+        await page.evaluate(() => {
+            document.body.classList.add('light-theme')
+            const focused = document
+                .getElementById('focused')
+                ?.shadowRoot?.querySelector('summary')
+            focused?.focus()
+        })
+        await expect(page).toHaveScreenshot()
+    })
 })
