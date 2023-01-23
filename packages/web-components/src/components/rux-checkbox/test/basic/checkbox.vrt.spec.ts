2685cb5c68da073be226a70554d6c6d4f84e87a4
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-01-23 15:59:43 -0500

    Checkbox hover VRT

--- packages/web-components/src/components/rux-checkbox/test/basic/checkbox.vrt.spec.ts
+++ packages/web-components/src/components/rux-checkbox/test/basic/checkbox.vrt.spec.ts
@@ -6,4 +6,9 @@ test.describe('Checkbox', () => {
     test('has no visual regression @vrt', async ({ astroVRTPage }) => {
         await expect(astroVRTPage).toHaveScreenshot()
     })
+    test('hover has no visual regression @vrt', async ({ astroVRTPage }) => {
+        const el = astroVRTPage.locator('rux-checkbox[data-test-id="default"]')
+        await el.hover()
+        await expect(astroVRTPage).toHaveScreenshot()
+    })
 })
