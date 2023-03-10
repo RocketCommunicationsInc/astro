59a69f2c3569c0dfb0053ace4f8f3594afb155a1
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-03-10 15:57:42 -0500

    test(accordion): reflection

---
+++ packages/web-components/src/components/rux-accordion/rux-accordion-item/test/accordion-item.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '../../../../../tests/utils/_astro-fixtures'
+
+test.describe('Accordion Item reflection', () => {
+    test.beforeEach(async ({ page }) => {
+        const template = `
+			<rux-accordion-item></rux-accordion-item>
+		`
+        await page.setContent(template)
+    })
+
+    test('the disabled property reflects to an attribute', async ({ page }) => {
+        const el = page.locator('rux-accordion-item')
+        await el.evaluate((accordionItemEl) => {
+            ;(accordionItemEl as HTMLRuxAccordionItemElement).disabled = true
+        })
+
+        await page.waitForChanges()
+
+        const attr = await el.evaluate((accordionItemEl) => {
+            return accordionItemEl.getAttribute('disabled')
+        })
+        expect(attr).not.toBeNull()
+    })
+
+    test('the expanded property reflects to an attribute', async ({ page }) => {
+        const el = page.locator('rux-accordion-item')
+        await el.evaluate((accordionItemEl) => {
+            ;(accordionItemEl as HTMLRuxAccordionElement).expanded = true
+        })
+
+        await page.waitForChanges()
+
+        const attr = await el.evaluate((accordionItemEl) => {
+            return accordionItemEl.getAttribute('expanded')
+        })
+        expect(attr).not.toBeNull()
+    })
+})
