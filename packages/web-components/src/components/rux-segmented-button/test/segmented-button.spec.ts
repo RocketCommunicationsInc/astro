451175b766e1a446cdafc2788640b5ef0b8236ad
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-05-15 10:53:53 -0400

    fix for rux-segmented button & test

--- packages/web-components/src/components/rux-segmented-button/test/segmented-button.spec.ts
+++ packages/web-components/src/components/rux-segmented-button/test/segmented-button.spec.ts
@@ -113,6 +113,41 @@ test.describe('Segmented-button', () => {
         await expect(changeEvent).toHaveReceivedEventTimes(1)
         await expect(changeEvent).toHaveReceivedEventDetail('Second segment')
     })
+    test('it properly reselects a pre-selected segment', async ({ page }) => {
+        const template = `
+        <div style="padding: 2.5% 5%">
+            <rux-segmented-button></rux-segmented-button>
+        </div>
+        `
+        await page.setContent(template)
+        await page.addScriptTag({
+            //first is selected by default
+            content: `
+            const segmented = document.querySelector('rux-segmented-button')
+            const data = [
+                { label: 'First segment' },
+                { label: 'Second segment' },
+                { label: 'Third segment' },
+            ]
+            segmented.data = data
+        `,
+        })
+        const segmentedButton = page.locator('rux-segmented-button')
+        const segButton1Segment = segmentedButton.locator('li').nth(1)
+        const segButton2Segment = segmentedButton.locator('li').nth(2)
+        const segButton1Input = segButton1Segment.locator('input')
+        //click second button
+        await segButton2Segment.click()
+        //make sure it changed
+        await expect(segButton2Segment.locator('input')).toBeChecked
+
+        //then programatically go back to first
+        await segmentedButton.evaluate((e) =>
+            e.setAttribute('selected', 'First segment')
+        )
+        //and make sure it gets checked
+        await expect(segButton1Input).toBeChecked
+    })
 })
 /*
     Need to test: 
