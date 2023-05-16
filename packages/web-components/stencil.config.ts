af2a8fbce236f558110624395f5b2c20f22253ca
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-05-16 13:14:20 -0400

    feat(core): add fallback values for css custom properties (#1132)
    * fresh
    
    * tests
    
    * add reset.css
    
    * revert icon
    
    * fix focus and input
    
    * postcss: cleanup
    
    * style(status): add fallback values to svgs
    
    * fix tests
    
    * more fixes
    
    * more fixes
    
    * test(postcss): add tests for single, multiple, calc
    
    * refactor(gsb): remove inline style
    
    * test(gsb): update vrts
    
    * fix tab/tag
    
    * fix(postcss): dont modify custom props if not in index
    
    * fix tab test
    
    * refactor: use published postcss plugin
    
    * build: no postcss in dev env
    
    * revert: wrong test file changed
    
    * test(push-button): restore test
    
    * chore: addFallbacks postcss only in prod build
    
    * chore: bump docker to jammy
    
    * test: remove timeout
    
    * chore: bump playwright
    
    * test: no timeout
    
    * fix: oopsie on workflow
    
    * chore: bump playwright/test
    
    * test(tooltip): fix expected style string
    
    * revert: revert playwright bump, remove timeout
    
    * Revert "test(tooltip): fix expected style string"
    
    This reverts commit d5bcbd75283ac7907d6c1a7b656a3ad8e087688a.
    
    * Revert "chore: bump playwright/test"
    
    This reverts commit 9606af8007136c91caf6a17a444906dc5a8e463f.
    
    * Revert "chore: bump playwright"
    
    This reverts commit 24dfccdb59e9b7a24910f14eb75c81c35ade06e7.
    
    * Revert "chore: bump docker to jammy"
    
    This reverts commit 25249d3359f3e2c8512ea4941d63c31c97af7c12.
    
    * chore: update snapshots
    
    * refactor: test cleanup
    
    * fix typo
    
    * docs: changeset
    
    ---------
    
    Co-authored-by: Micah Jones <micah.jones@rocketcom.com>

--- packages/web-components/stencil.config.ts
+++ packages/web-components/stencil.config.ts
@@ -5,7 +5,33 @@ import { angularOutputTarget } from '@stencil/angular-output-target'
 import { angularValueAccessorBindings } from './wrapper-bindings/angular.bindings'
 import { reactOutputTarget } from '@stencil/react-output-target'
 import { reactBooleanFix } from './wrapper-bindings/react-boolean-fix'
-
+import { postcss } from '@stencil/postcss'
+import addFallbacks from '@astrouxds/postcss-custom-property-token-fallback'
+import reference from '@astrouxds/tokens/dist/json/base.reference.json'
+import system from '@astrouxds/tokens/dist/json/base.system.json'
+import component from '@astrouxds/tokens/dist/json/base.component.json'
+//misc global vars that have yet to be made tokens
+const oneOffs = {
+    'indeterminate-gradient':
+        'conic-gradient(rgba(77, 172, 255, 0), rgba(51, 51, 51, 0), rgba(77, 172, 255, 0.3), rgba(77, 172, 255, 0.6), rgba(77, 172, 255, 0.7), rgb(77, 172, 255))',
+    'indeterminate-nub-color': '#4dacff',
+    'standby-fill': '#2dccff',
+    'standby-border': '#2dccff',
+    'standby-fill-opacity': '0',
+    'critical-fill': '#ff3838',
+    'critical-border': '#ff3838',
+    'serious-fill': '#ffb302',
+    'serious-border': '#ffb302',
+    'caution-fill': '#fce83a',
+    'caution-border': '#fce83a',
+    'normal-fill': '#56f000',
+    'normal-border': '#56f000',
+    'off-fill': '#a4abb6',
+    'off-border': '#a4abb6',
+    'container-footer-color-background': '#172635',
+    'container-header-color-background': '#172635',
+}
+const tokens = Object.assign({}, reference, system, component, oneOffs)
 export const config: Config = {
     namespace: 'astro-web-components',
     globalStyle: 'src/global/global.scss',
@@ -56,7 +82,13 @@ export const config: Config = {
             ],
         },
     ],
-    plugins: [sass(), svgOptimizerPlugin()],
+    plugins: [
+        sass(),
+        svgOptimizerPlugin(),
+        postcss({
+            plugins: process.env.PROD ? [addFallbacks({ index: tokens })] : [],
+        }),
+    ],
     enableCache: true,
     extras: {
         appendChildSlotFix: true,
