af17dcdd88da44739dba3809c82c88039e132eae
Micah Jones <micah.jones@rocketcom.com>
2023-05-23 09:22:08 -0600

    feat(select): borderless variant

--- packages/web-components/src/components/rux-select/rux-select.tsx
+++ packages/web-components/src/components/rux-select/rux-select.tsx
@@ -102,6 +102,11 @@ export class RuxSelect implements FormFieldInterface {
     @Prop({ reflect: true }) size?: 'small' | 'medium' | 'large' = 'medium'
 
     /**
+     * Styles the select menu as a borderless, inline variant.
+     */
+    @Prop({ reflect: true }) borderless?: boolean = false
+
+    /**
      * Event Emitted when the Value of the Select is Changed
      */
     @Event({ eventName: 'ruxchange' })
@@ -344,6 +349,7 @@ export class RuxSelect implements FormFieldInterface {
                         'rux-select--large': this.size === 'large',
                         'rux-select--invalid': invalid,
                         'rux-select--multiple': multiple,
+                        'rux-select--borderless': this.borderless === true,
                     }}
                     ref={(el) => (this.selectEl = el as HTMLSelectElement)}
                     id={inputId}
