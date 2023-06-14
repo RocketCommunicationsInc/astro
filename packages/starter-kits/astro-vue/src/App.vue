c7c74343889a39f8c282ead6849708045fc83c08
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-06-14 16:26:26 -0400

    tailwind

--- packages/starter-kits/astro-vue/src/App.vue
+++ packages/starter-kits/astro-vue/src/App.vue
@@ -1,31 +1,15 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <rux-button>ok</rux-button>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <rux-global-status-bar app-name="Astro"></rux-global-status-bar>
+  <main class="grid gap-4 p-4 grid-cols-1 lg:grid-cols-12">
+    <rux-container class="lg:col-span-2">
+      <header slot="header">Header</header>
+    </rux-container>
+    <rux-container class="lg:col-span-10">
+      <header slot="header">Header</header>
+    </rux-container>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
