083c3cf5939fbd140ae5bc549bf838f88b76897a
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-06-14 16:21:19 -0400

    tailwind

--- packages/starter-kits/astro-react/src/App.tsx
+++ packages/starter-kits/astro-react/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { RuxButton } from "@astrouxds/react";
+import { RuxContainer, RuxGlobalStatusBar } from "@astrouxds/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <RuxButton>yo</RuxButton>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RuxGlobalStatusBar appName="Astro" />
+      <main className="grid gap-4 p-4 grid-cols-1 lg:grid-cols-12">
+        <RuxContainer className="lg:col-span-2">
+          <header slot="header">Header</header>
+        </RuxContainer>
+
+        <RuxContainer className="lg:col-span-10">
+          <header slot="header">Header</header>
+        </RuxContainer>
+      </main>
     </>
   );
 }
