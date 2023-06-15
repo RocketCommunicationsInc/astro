aaddcc041d636e69a60fa66c35283e682388ef66
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-06-15 15:43:41 -0400

    add angular commands

--- packages/starter-kits/angular-starter/src/app/app.module.ts
+++ packages/starter-kits/angular-starter/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -9,5 +9,6 @@ import { AstroComponentsModule } from '@astrouxds/angular';
   imports: [BrowserModule, AstroComponentsModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
