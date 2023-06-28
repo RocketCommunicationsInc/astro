465f175a357c6932e4924403b029e14982e72942
Kiley Mitti <kiley.mitti@rocketcom.com>
2023-06-28 15:43:56 -0400

    add size prop

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -12089,6 +12089,10 @@ export namespace Components {
          */
         "notifications": number;
         /**
+          * The size of a chosen Astro icon. Can be 'extra-small', 'small', 'normal', 'large', 'auto' or any custom value ('30px', '1rem', '3.321em')
+         */
+        "size": string;
+        /**
           * Styles the icon according to the Astro Status colors. Valid options are the Astro statuses `critical`, `serious`, `caution`, `normal`, `standby`, and `off`.
          */
         "status": Status;
@@ -32717,6 +32721,10 @@ declare namespace LocalJSX {
           * If provided and greater than `0`, displays an outlined number badge at the bottom right of the icon. Numbers above `9999` are abbreviated to `'10K'` or `'100K'` for numbers in the thousands, `'1.5M'` for millions, `'1.5B'` for billions, and uses `'∞'` for one trillion or higher.
          */
         "notifications"?: number;
+        /**
+          * The size of a chosen Astro icon. Can be 'extra-small', 'small', 'normal', 'large', 'auto' or any custom value ('30px', '1rem', '3.321em')
+         */
+        "size"?: string;
         /**
           * Styles the icon according to the Astro Status colors. Valid options are the Astro statuses `critical`, `serious`, `caution`, `normal`, `standby`, and `off`.
          */
