0b5658e88abe40d7b32d45981881f1ee746e6369
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-06-29 10:28:34 -0400

    fixing merge conflicts

--- packages/react/src/react-component-lib/utils/attachProps.ts
+++ packages/react/src/react-component-lib/utils/attachProps.ts
@@ -70,17 +70,6 @@ export const getClassName = (classList: DOMTokenList, newProps: any, oldProps: a
 };
 
 /**
- * Transforms a React event name to a browser event name.
- */
-export const transformReactEventName = (eventNameSuffix: string) => {
-  switch (eventNameSuffix) {
-    case 'doubleclick':
-      return 'dblclick';
-  }
-  return eventNameSuffix;
-};
-
-/**
  * Checks if an event is supported in the current execution environment.
  * @license Modernizr 3.0.0pre (Custom Build) | MIT
  */
@@ -88,7 +77,7 @@ export const isCoveredByReact = (eventNameSuffix: string) => {
   if (typeof document === 'undefined') {
     return true;
   } else {
-    const eventName = 'on' + transformReactEventName(eventNameSuffix);
+    const eventName = 'on' + eventNameSuffix;
     let isSupported = eventName in document;
 
     if (!isSupported) {
