24930297f0a6847bd3ab72e668bfef498036bf85
Mark Anthony Cianfrani <markacianf@gmail.com>
2023-07-11 11:20:40 -0400

    test: add testName option

--- packages/web-components/tests/utils/_astro-fixtures.ts
+++ packages/web-components/tests/utils/_astro-fixtures.ts
@@ -8,14 +8,16 @@ import { Page } from "@playwright/test";
 export type TestOptions = {
     theme: 'dark' | 'light';
     component: string
+    testName: string
     astroVRTPage: Page
   };
 
 export const test = base.extend<TestOptions>({
     theme: ['dark', { option: true }],
     component: ['rux-foo', { option: true }],
-    astroVRTPage: async ({ theme, component, page }, use) => {
-        await page.goto(`/src/components/${component}/test/basic/index.html`)
+    testName: ['basic', { option: true}],
+    astroVRTPage: async ({ theme, component, testName, page }, use) => {
+        await page.goto(`/src/components/${component}/test/${testName}/index.html`)
         // Set up the fixture.
         if (theme === 'light') {
                 await page.evaluate(() => {
