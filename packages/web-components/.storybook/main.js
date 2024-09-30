94efde550c30bb351d31bae35a71778cb731cfce
Frances Morrison <frances.morrison87@gmail.com>
2024-09-30 10:41:16 -0400

    nit: removing storybook option

--- packages/web-components/.storybook/main.js
+++ packages/web-components/.storybook/main.js
@@ -1,5 +1,3 @@
-import remarkGfm from 'remark-gfm';
-
 module.exports = {
   stories: ['../src/stories/astro-uxds/StartHere.mdx', '../src/stories/**/*.mdx', '../src/stories/*.stories.@(js|jsx|ts|tsx)'],
 
