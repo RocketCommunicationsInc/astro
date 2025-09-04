97b4c3575f0690369479c2552f301ad4e687c3d9
Gwen Wendell <gwen@thecodingdesigner.com>
2025-09-04 15:47:47 -0400

    refactor: simplify rux-feedback component by removing unused state and slot handling

--- packages/web-components/src/components/rux-feedback/rux-feedback.tsx
+++ packages/web-components/src/components/rux-feedback/rux-feedback.tsx
@@ -20,55 +20,90 @@ import { hasSlot } from '../../utils/utils'
 export class RuxFeedback {
     @Element() el!: HTMLRuxFeedbackElement
 
-    @State() activeSlots = {
-        header: false,
-        footer: false,
-    }
-
-    connectedCallback() {
-        this._handleSlotChange = this._handleSlotChange.bind(this)
-    }
-
-    private _handleSlotChange(slotName: 'header' | 'footer') {
-        const show = hasSlot(this.el, slotName)
-        this.activeSlots = { ...this.activeSlots, [slotName]: show }
-    }
+    // @State() activeSlots = {
+    //     header: false,
+    //     footer: false,
+    // }
 
     render() {
         return (
             <Host>
                 <div class="rux-feedback" part="container">
-                    <div
-                        class={{
-                            'rux-feedback__header': true,
-                            hidden: !this.activeSlots.header,
-                        }}
-                        part="header"
-                    >
-                        <slot
-                            name="header"
-                            onSlotchange={() =>
-                                this._handleSlotChange('header')
-                            }
-                        ></slot>
+                    <div class="rux-feedback__header">
+                        <rux-classification-marking classification="unclassified"></rux-classification-marking>
+                        <h3 class="rux-feedback__title">
+                            Send us your feedback
+                        </h3>
                     </div>
 
-                    <div class="rux-feedback__body" part="body">
-                        <slot></slot>
+                    <div class="rux-feedback__form">
+                        <div class="rux-form__topic-group">
+                            <rux-button
+                                size="large"
+                                icon="error-outline"
+                                secondary
+                            >
+                                Issue
+                            </rux-button>
+                            <rux-button size="large" icon="highlight" secondary>
+                                Idea
+                            </rux-button>
+                            <rux-button
+                                size="large"
+                                icon="bug-report"
+                                secondary
+                            >
+                                Bug
+                            </rux-button>
+                            <rux-button size="large" icon="textsms" secondary>
+                                Other
+                            </rux-button>
+                        </div>
+
+                        <div class="rux-form-topic__issue">
+                            <rux-input
+                                label="What page are you commenting on?"
+                                placeholder="Add URL"
+                                type="url"
+                                size="medium"
+                            ></rux-input>
+                            <rux-textarea label="Describe your issue"></rux-textarea>
+                        </div>
+
+                        <div class="rux-form-topic__idea">
+                            <rux-textarea label="Tell us about your idea"></rux-textarea>
+                        </div>
+
+                        <div class="rux-form-topic__bug">
+                            <rux-input
+                                label="What page is this bug on?"
+                                placeholder="Add URL"
+                                type="url"
+                                size="medium"
+                            ></rux-input>
+                            <rux-input
+                                label="Browser"
+                                type="text"
+                                size="medium"
+                            ></rux-input>
+                            <rux-textarea label="Describe the bug"></rux-textarea>
+                        </div>
+
+                        <div class="rux-form-topic__other">
+                            <h3>Sentiment picker</h3>
+                            <rux-textarea label="Describe your experience using tktk-platform-Acme"></rux-textarea>
+                        </div>
+
+                        <rux-button secondary>Cancel</rux-button>
+                        <rux-button>Send</rux-button>
                     </div>
-                    <div
-                        class={{
-                            'rux-feedback__footer': true,
-                            hidden: !this.activeSlots.footer,
-                        }}
-                        part="footer"
-                    >
-                        <slot
-                            name="footer"
-                            onSlotchange={() =>
-                                this._handleSlotChange('footer')
-                            }
-                        ></slot>
+
+                    <div class="rux-feedback__success">
+                        <h3>Thank you.</h3>
+                        <rux-icon size="normal" icon="check"></rux-icon>
+                        <p>Your feedback was sent.</p>
+                        <p>Send More Feedback?</p>
+                        <rux-button>Close</rux-button>
                     </div>
                 </div>
             </Host>
