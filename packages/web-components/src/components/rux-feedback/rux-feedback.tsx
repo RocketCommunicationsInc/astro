704876e8b168b96fa32c44b44bce3737be4db6b6
Gwen Wendell <gwen@thecodingdesigner.com>
2025-09-19 16:59:03 -0400

    feat: enhance rux-feedback component with improved layout and dynamic topic buttons

--- packages/web-components/src/components/rux-feedback/rux-feedback.tsx
+++ packages/web-components/src/components/rux-feedback/rux-feedback.tsx
@@ -34,51 +34,89 @@ export class RuxFeedback {
         return (
             <Host>
                 <div class="rux-feedback" part="container">
-                    <div class="rux-feedback__header">
+                    <div class="rux-feedback__tab" part="tab">
+                        Feedback
+                        <rux-icon
+                            icon="keyboard-arrow-up"
+                            size="small"
+                        ></rux-icon>
+                    </div>
+                    <div class="rux-feedback__header" part="header">
                         <rux-classification-marking classification="unclassified"></rux-classification-marking>
-                        <h3 class="rux-feedback__title">
+                        <h3 class="rux-feedback__title" part="title">
                             Send us your feedback
                         </h3>
                     </div>
                     {this.activeTopic !== 'success' && (
-                        <div class="rux-feedback__form">
-                            <div class="rux-form__topic-group">
-                                <rux-button
-                                    size="large"
-                                    icon="error-outline"
-                                    secondary
+                        <div class="rux-feedback__form" part="form">
+                            <div
+                                class="rux-form__topic-group"
+                                part="topic-group"
+                            >
+                                <div
+                                    class={`rux-form__topic-button ${
+                                        this.activeTopic === 'issue'
+                                            ? 'active'
+                                            : ''
+                                    }`}
                                     onClick={this.handleIssueClick}
                                 >
-                                    Issue
-                                </rux-button>
-                                <rux-button
-                                    size="large"
-                                    icon="highlight"
-                                    secondary
+                                    <rux-icon
+                                        class="topic-button__icon"
+                                        icon="error-outline"
+                                        size="20px"
+                                    ></rux-icon>
+                                    <div class="topic-button__label">Issue</div>
+                                </div>
+                                <div
+                                    class={`rux-form__topic-button ${
+                                        this.activeTopic === 'idea'
+                                            ? 'active'
+                                            : ''
+                                    }`}
                                     onClick={this.handleIdeaClick}
                                 >
-                                    Idea
-                                </rux-button>
-                                <rux-button
-                                    size="large"
-                                    icon="bug-report"
-                                    secondary
+                                    <rux-icon
+                                        class="topic-button__icon"
+                                        icon="highlight"
+                                        size="20px"
+                                    ></rux-icon>
+                                    <div class="topic-button__label">Idea</div>
+                                </div>
+                                <div
+                                    class={`rux-form__topic-button ${
+                                        this.activeTopic === 'bug'
+                                            ? 'active'
+                                            : ''
+                                    }`}
                                     onClick={this.handleBugClick}
                                 >
-                                    Bug
-                                </rux-button>
-                                <rux-button
-                                    size="large"
-                                    icon="textsms"
-                                    secondary
+                                    <rux-icon
+                                        class="topic-button__icon"
+                                        icon="bug-report"
+                                        size="20px"
+                                    ></rux-icon>
+                                    <div class="topic-button__label">Bug</div>
+                                </div>
+                                <div
+                                    class={`rux-form__topic-button ${
+                                        this.activeTopic === 'other'
+                                            ? 'active'
+                                            : ''
+                                    }`}
                                     onClick={this.handleOtherClick}
                                 >
-                                    Other
-                                </rux-button>
+                                    <rux-icon
+                                        class="topic-button__icon"
+                                        icon="textsms"
+                                        size="20px"
+                                    ></rux-icon>
+                                    <div class="topic-button__label">Other</div>
+                                </div>
                             </div>
 
                             {this.activeTopic === 'issue' && (
-                                <div class="rux-form-topic__issue">
+                                <div class="rux-form-topic__issue" part="issue">
                                     <rux-input
                                         label="What page are you commenting on?"
                                         placeholder="Add URL"
@@ -89,12 +127,12 @@ export class RuxFeedback {
                                 </div>
                             )}
                             {this.activeTopic === 'idea' && (
-                                <div class="rux-form-topic__idea">
+                                <div class="rux-form-topic__idea" part="idea">
                                     <rux-textarea label="Tell us about your idea"></rux-textarea>
                                 </div>
                             )}
                             {this.activeTopic === 'bug' && (
-                                <div class="rux-form-topic__bug">
+                                <div class="rux-form-topic__bug" part="bug">
                                     <rux-input
                                         label="What page is this bug on?"
                                         placeholder="Add URL"
@@ -110,7 +148,7 @@ export class RuxFeedback {
                                 </div>
                             )}
                             {this.activeTopic === 'other' && (
-                                <div class="rux-form-topic__other">
+                                <div class="rux-form-topic__other" part="other">
                                     <h3>Sentiment picker</h3>
                                     <rux-textarea label="Describe your experience using tktk-platform-Acme"></rux-textarea>
                                 </div>
@@ -122,7 +160,7 @@ export class RuxFeedback {
                     )}
 
                     {this.activeTopic === 'success' && (
-                        <div class="rux-feedback__success">
+                        <div class="rux-feedback__success" part="success">
                             <h3>Thank you.</h3>
                             <rux-icon size="normal" icon="check"></rux-icon>
                             <p>Your feedback was sent.</p>
