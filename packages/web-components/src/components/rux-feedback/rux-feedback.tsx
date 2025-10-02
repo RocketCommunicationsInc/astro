92064e9c253624fd1ea2f3ccdf4b84e25936c2f4
Gwen Wendell <gwen@thecodingdesigner.com>
2025-10-02 12:30:40 -0400

    feat: enhance rux-feedback component with improved form handling and rendering

--- packages/web-components/src/components/rux-feedback/rux-feedback.tsx
+++ packages/web-components/src/components/rux-feedback/rux-feedback.tsx
@@ -9,19 +9,28 @@ import { Classification } from '../../common/commonTypes.module'
 export class RuxFeedback {
     @Element() el!: HTMLRuxFeedbackElement
 
+    // Optional classification marking
+    // e.g. U, CUI, SBU, etc.
+    // See rux-classification-marking for full list of options
+    // If no classification is set, no marking will be displayed
+    // (default behavior)
     @Prop({ reflect: true }) classification?: Classification = undefined
 
+    // Active feedback topic pane
     @State() activeTopic: 'issue' | 'idea' | 'bug' | 'other' | 'success' =
         'issue'
 
+    // Active sentiment option
     @State() activeSentiment:
         | 'positive'
         | 'neutral'
         | 'negative'
         | 'confusing' = 'positive'
 
+    // Is feedback form open or closed
     @State() isOpen: boolean = true
 
+    // PRIMARY UI BUTTONS
     // Toggle feedback form open/closed
     private handleTabClick = () => {
         this.isOpen = !this.isOpen
@@ -32,7 +41,12 @@ export class RuxFeedback {
             feedbackEl?.classList.remove('open')
         }
     }
-
+    // Handle cancel button click
+    private handleCancel = (e: Event) => {
+        e.preventDefault()
+        this.handleClose()
+    }
+    // Handle close button click
     private handleClose = () => {
         this.isOpen = false
         const feedbackEl = this.el.shadowRoot?.querySelector('.rux-feedback')
@@ -41,19 +55,14 @@ export class RuxFeedback {
         this.sentimentSwitcher('positive')
     }
 
-    private handleSendMoreFeedback = (e: Event) => {
-        e.preventDefault()
-        this.paneSwitcher('issue')
-        this.sentimentSwitcher('positive')
-    }
-
+    // FORM SWITCHING
     // Switch between feedback topic panes
     private paneSwitcher(
         topic: 'issue' | 'idea' | 'bug' | 'other' | 'success'
     ) {
         this.activeTopic = topic
     }
-
+    // Handle topic button click
     private handleTopicClick = (event: Event) => {
         const target = event.currentTarget as HTMLElement
         const topic = target.getAttribute('data-topic') as
@@ -66,13 +75,13 @@ export class RuxFeedback {
         }
     }
 
+    // SENTIMENT SWITCHING
     // Switch between sentiment options
     private sentimentSwitcher(
         sentiment: 'positive' | 'neutral' | 'negative' | 'confusing'
     ) {
         this.activeSentiment = sentiment
     }
-
     // Handle sentiment button click
     private handleSentimentClick = (event: Event) => {
         const target = event.currentTarget as HTMLElement
@@ -101,56 +110,14 @@ export class RuxFeedback {
         )
     }
 
-    render() {
-        return (
-            <Host>
-                <div
-                    class={`rux-feedback ${this.isOpen ? 'open' : ''}`}
-                    part="container"
-                >
-                    {this.renderFeedbackTab()}
-                    <div class="rux-feedback__header" part="header">
-                        {this.classification && (
-                            <rux-classification-marking
-                                classification={this.classification}
-                            ></rux-classification-marking>
-                        )}
-                        <h3 class="rux-feedback__title" part="title">
-                            Send us your feedback
-                        </h3>
-                    </div>
-                    {this.activeTopic !== 'success' && (
-                        <form class="rux-feedback__form" part="form">
-                            {this.renderTopicSelector()}
-
-                            {this.activeTopic === 'issue' &&
-                                this.renderIssueForm()}
-
-                            {this.activeTopic === 'idea' &&
-                                this.renderIdeaForm()}
-
-                            {this.activeTopic === 'bug' &&
-                                this.renderBugReportForm()}
-
-                            {this.activeTopic === 'other' &&
-                                this.renderOtherFeedbackForm()}
-
-                            <div class="rux-form__buttons">
-                                <rux-button secondary>Cancel</rux-button>
-                                <rux-button onClick={this.handleSubmit}>
-                                    Send
-                                </rux-button>
-                            </div>
-                        </form>
-                    )}
-
-                    {this.activeTopic === 'success' &&
-                        this.renderSuccessMessage()}
-                </div>
-            </Host>
-        )
+    // Handle "Send More Feedback" link click
+    private handleSendMoreFeedback = (e: Event) => {
+        e.preventDefault()
+        this.paneSwitcher('issue')
+        this.sentimentSwitcher('positive')
     }
 
+    // RENDERERS
     // Feedback tab that toggles the form open/closed
     private renderFeedbackTab() {
         console.log('isOpen', this.isOpen)
@@ -344,6 +311,61 @@ export class RuxFeedback {
                     <rux-button onClick={this.handleClose}>Close</rux-button>
                 </div>
             </div>
+        )
+    }
+
+    render() {
+        return (
+            <Host>
+                <div
+                    class={`rux-feedback ${this.isOpen ? 'open' : ''}`}
+                    part="container"
+                >
+                    {this.renderFeedbackTab()}
+                    <div class="rux-feedback__header" part="header">
+                        {this.classification && (
+                            <rux-classification-marking
+                                classification={this.classification}
+                            ></rux-classification-marking>
+                        )}
+                        <h3 class="rux-feedback__title" part="title">
+                            Send us your feedback
+                        </h3>
+                    </div>
+                    {this.activeTopic !== 'success' && (
+                        <form class="rux-feedback__form" part="form">
+                            {this.renderTopicSelector()}
+
+                            {this.activeTopic === 'issue' &&
+                                this.renderIssueForm()}
+
+                            {this.activeTopic === 'idea' &&
+                                this.renderIdeaForm()}
+
+                            {this.activeTopic === 'bug' &&
+                                this.renderBugReportForm()}
+
+                            {this.activeTopic === 'other' &&
+                                this.renderOtherFeedbackForm()}
+
+                            <div class="rux-form__buttons">
+                                <rux-button
+                                    secondary
+                                    onClick={this.handleCancel}
+                                >
+                                    Cancel
+                                </rux-button>
+                                <rux-button onClick={this.handleSubmit}>
+                                    Send
+                                </rux-button>
+                            </div>
+                        </form>
+                    )}
+
+                    {this.activeTopic === 'success' &&
+                        this.renderSuccessMessage()}
+                </div>
+            </Host>
         )
     }
 }
