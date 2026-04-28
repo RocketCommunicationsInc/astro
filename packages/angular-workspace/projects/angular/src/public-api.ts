1a52b275a60afc2af22d6febdeb928ec18a64e8d
Nic Dominguez <NicDominguez@users.noreply.github.com>
2026-04-28 12:54:53 -0700

    remove dead accessors

--- packages/angular-workspace/projects/angular/src/public-api.ts
+++ packages/angular-workspace/projects/angular/src/public-api.ts
@@ -6,8 +6,6 @@ export { ProxyCmp as ɵa } from './directives/angular-component-lib/utils';
 export * from './lib/angular-lib.service';
 export * from './lib/angular-lib.component';
 export * from './directives/proxies';
-export { RadioValueAccessor } from './directives/radio-value-accessor';
-export { SelectValueAccessor } from './directives/select-value-accessor';
 export { BooleanValueAccessor } from './directives/boolean-value-accessor';
 export { TextValueAccessor } from './directives/text-value-accessor';
 export { NumericValueAccessor } from './directives/number-value-accessor';
